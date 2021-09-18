3828674ee9d495103af847d8399ba52185051d37
Zsolt Kocsi <zsolt.kocsi@team.bumble.com>
2021-09-18 20:15:20 +0100

    Add more colors

--- app/src/main/java/com/github/zsoltk/composeribs/client/child/ChildNode.kt
+++ app/src/main/java/com/github/zsoltk/composeribs/client/child/ChildNode.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,20 +20,35 @@ class ChildNode(
     private val i: Int
 ) : LeafNode() {
 
+    private val colors = listOf(
+        manatee,
+        sizzling_red,
+        atomic_tangerine,
+        silver_sand,
+        md_pink_500,
+        md_indigo_500,
+        md_blue_500,
+        md_light_blue_500,
+        md_cyan_500,
+        md_teal_500,
+        md_light_green_500,
+        md_lime_500,
+        md_amber_500,
+        md_grey_500,
+        md_blue_grey_500
+    )
+
+    private val color = colors.random()
+
     @Composable
     override fun View() {
-        Box(modifier = Modifier
-            .fillMaxSize()
-            .background(
-                color = when (i % 4) {
-                    0 -> manatee
-                    1 -> sizzling_red
-                    2 -> atomic_tangerine
-                    3 -> silver_sand
-                    else -> Color.Unspecified
-                },
-                shape = RoundedCornerShape(6.dp)
-            )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    color = color,
+                    shape = RoundedCornerShape(6.dp)
+                )
         ) {
             Column(
                 modifier = Modifier.padding(24.dp)
--- app/src/main/java/com/github/zsoltk/composeribs/ui/Color.kt
+++ app/src/main/java/com/github/zsoltk/composeribs/ui/Color.kt
@@ -2,7 +2,6 @@ package com.github.zsoltk.composeribs.ui
 
 import androidx.compose.ui.graphics.Color
 
-
 val purple200 = Color(0xFFBB86FC)
 val purple500 = Color(0xFF6200EE)
 val purple700 = Color(0xFF3700B3)
