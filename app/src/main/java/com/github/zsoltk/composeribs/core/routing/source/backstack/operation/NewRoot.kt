0959b628d408682cef4bf2a1216eac73602bab2b
Gael Marhic <gael.marhic@team.bumble.com>
2021-10-25 16:34:31 +0200

    Add NewRoot operation

---
+++ app/src/main/java/com/github/zsoltk/composeribs/core/routing/source/backstack/operation/NewRoot.kt
@@ -0,0 +1,35 @@
+package com.github.zsoltk.composeribs.core.routing.source.backstack.operation
+
+import com.github.zsoltk.composeribs.core.routing.source.backstack.BackStack
+import com.github.zsoltk.composeribs.core.routing.source.backstack.BackStack.Operation
+import com.github.zsoltk.composeribs.core.routing.source.backstack.BackStackElement
+import com.github.zsoltk.composeribs.core.routing.source.backstack.UuidGenerator
+import com.github.zsoltk.composeribs.core.routing.source.backstack.current
+
+/**
+ * Operation:
+ *
+ * [A, B, C] + NewRoot(D) = [ D ]
+ */
+internal class NewRoot<T>(
+    private val element: T
+) : Operation<T> {
+
+    override fun isApplicable(elements: List<BackStackElement<T>>): Boolean =
+        !(elements.size == 1 && elements.current?.key?.routing == element)
+
+    override fun invoke(
+        elements: List<BackStackElement<T>>,
+        uuidGenerator: UuidGenerator
+    ): List<BackStackElement<T>> = listOf(
+        BackStackElement(
+            key = BackStack.LocalRoutingKey(element, uuidGenerator.incrementAndGet()),
+            fromState = BackStack.TransitionState.CREATED,
+            targetState = BackStack.TransitionState.ON_SCREEN,
+        )
+    )
+}
+
+fun <T> BackStack<T>.newRoot(element: T) {
+    perform(NewRoot(element))
+}
