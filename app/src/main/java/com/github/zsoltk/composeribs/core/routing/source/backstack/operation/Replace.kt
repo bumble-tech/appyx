20cfb92f7002998448746d1f1a6db1a564a11aae
Gael Marhic <gael.marhic@team.bumble.com>
2021-10-25 17:03:14 +0200

    Add Replace operation

---
+++ app/src/main/java/com/github/zsoltk/composeribs/core/routing/source/backstack/operation/Replace.kt
@@ -0,0 +1,32 @@
+package com.github.zsoltk.composeribs.core.routing.source.backstack.operation
+
+import com.github.zsoltk.composeribs.core.routing.source.backstack.BackStack
+import com.github.zsoltk.composeribs.core.routing.source.backstack.BackStackElement
+import com.github.zsoltk.composeribs.core.routing.source.backstack.UuidGenerator
+import com.github.zsoltk.composeribs.core.routing.source.backstack.current
+
+/**
+ * Operation:
+ *
+ * [A, B, C] + Replace(D) = [A, B, D]
+ */
+internal class Replace<T>(
+    private val element: T
+) : BackStack.Operation<T> {
+
+    override fun isApplicable(elements: List<BackStackElement<T>>): Boolean =
+        element != elements.current?.key?.routing
+
+    override fun invoke(
+        elements: List<BackStackElement<T>>,
+        uuidGenerator: UuidGenerator
+    ): List<BackStackElement<T>> = elements.dropLast(1) + BackStackElement(
+        key = BackStack.LocalRoutingKey(element, uuidGenerator.incrementAndGet()),
+        fromState = BackStack.TransitionState.CREATED,
+        targetState = BackStack.TransitionState.ON_SCREEN,
+    )
+}
+
+fun <T> BackStack<T>.replace(element: T) {
+    perform(Replace(element))
+}
