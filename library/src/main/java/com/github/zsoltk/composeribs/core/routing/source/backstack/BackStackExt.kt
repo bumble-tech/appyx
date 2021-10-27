aa4c22b74a520f7a63c1340cafd680465e8fd0aa
Gael Marhic <gael.marhic@team.bumble.com>
2021-10-27 17:30:00 +0200

    Fix current extension property

--- library/src/main/java/com/github/zsoltk/composeribs/core/routing/source/backstack/BackStackExt.kt
+++ library/src/main/java/com/github/zsoltk/composeribs/core/routing/source/backstack/BackStackExt.kt
@@ -1,4 +1,4 @@
 package com.github.zsoltk.composeribs.core.routing.source.backstack
 
 val <T> Elements<T>.current: BackStackElement<T>?
-    get() = this.lastOrNull()
+    get() = this.lastOrNull { it.targetState == BackStack.TransitionState.ON_SCREEN }
