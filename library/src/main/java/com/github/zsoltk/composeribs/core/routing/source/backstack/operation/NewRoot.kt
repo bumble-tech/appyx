5fad52e7a2704132660b8b09947eca0d515ea728
Gael Marhic <gael.marhic@team.bumble.com>
2021-10-27 14:32:41 +0200

    Fix logic in NewRoot operation

--- library/src/main/java/com/github/zsoltk/composeribs/core/routing/source/backstack/operation/NewRoot.kt
+++ library/src/main/java/com/github/zsoltk/composeribs/core/routing/source/backstack/operation/NewRoot.kt
@@ -1,7 +1,10 @@
 package com.github.zsoltk.composeribs.core.routing.source.backstack.operation
 
-import com.github.zsoltk.composeribs.core.routing.source.backstack.*
+import com.github.zsoltk.composeribs.core.routing.source.backstack.BackStack
 import com.github.zsoltk.composeribs.core.routing.source.backstack.BackStack.Operation
+import com.github.zsoltk.composeribs.core.routing.source.backstack.BackStackElement
+import com.github.zsoltk.composeribs.core.routing.source.backstack.Elements
+import com.github.zsoltk.composeribs.core.routing.source.backstack.UuidGenerator
 
 /**
  * Operation:
@@ -12,25 +15,28 @@ internal class NewRoot<T : Any>(
     private val element: T
 ) : Operation<T> {
 
-    override fun isApplicable(elements: Elements<T>): Boolean =
-        !(elements.size == 1 && elements.current?.key?.routing == element)
+    override fun isApplicable(elements: Elements<T>): Boolean = true
 
     override fun invoke(
         elements: Elements<T>,
         uuidGenerator: UuidGenerator
     ): Elements<T> {
-        val last = elements.lastOrNull()?.copy(targetState = BackStack.TransitionState.DESTROYED)
 
+        val last = elements.lastOrNull()
         requireNotNull(last) { "No previous elements, state=$elements" }
 
-        return listOf(
-            last,
-            BackStackElement(
-                key = BackStack.LocalRoutingKey(element, uuidGenerator.incrementAndGet()),
-                fromState = BackStack.TransitionState.CREATED,
-                targetState = BackStack.TransitionState.ON_SCREEN,
+        return if (last.key.routing == element) {
+            listOf(elements.last())
+        } else {
+            listOf(
+                last.copy(targetState = BackStack.TransitionState.DESTROYED),
+                BackStackElement(
+                    key = BackStack.LocalRoutingKey(element, uuidGenerator.incrementAndGet()),
+                    fromState = BackStack.TransitionState.CREATED,
+                    targetState = BackStack.TransitionState.ON_SCREEN,
+                )
             )
-        )
+        }
     }
 }
 
