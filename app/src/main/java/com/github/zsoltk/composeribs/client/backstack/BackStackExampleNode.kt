1229ad888a0d3a6d565d5ec2ac1361eb705a163c
Gael Marhic <gael.marhic@team.bumble.com>
2021-10-28 13:02:46 +0200

    Show BackStack state on screen

--- app/src/main/java/com/github/zsoltk/composeribs/client/backstack/BackStackExampleNode.kt
+++ app/src/main/java/com/github/zsoltk/composeribs/client/backstack/BackStackExampleNode.kt
@@ -5,7 +5,10 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.RadioButton
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -17,6 +20,7 @@ import com.github.zsoltk.composeribs.core.SavedStateMap
 import com.github.zsoltk.composeribs.core.Subtree
 import com.github.zsoltk.composeribs.core.routing.source.backstack.BackStack
 import com.github.zsoltk.composeribs.core.routing.source.backstack.BackStackSlider
+import com.github.zsoltk.composeribs.core.routing.source.backstack.Elements
 import com.github.zsoltk.composeribs.core.routing.source.backstack.operation.pop
 import com.github.zsoltk.composeribs.core.routing.source.backstack.operation.push
 import kotlinx.parcelize.Parcelize
@@ -45,8 +49,10 @@ class BackStackExampleNode(
     @Composable
     override fun View() {
         Column(modifier = Modifier.fillMaxSize()) {
-            Text("Back stack example placeholder")
+            val backStackState = backStack.all.collectAsState()
+            val selectedRadioButton = rememberSaveable { mutableStateOf("A") }
 
+            Text("Back stack example placeholder")
             Column(Modifier.padding(24.dp)) {
                 Box(
                     Modifier
@@ -62,8 +68,7 @@ class BackStackExampleNode(
                         }
                     }
                 }
-
-                val selectedRadioButton = rememberSaveable { mutableStateOf("A") }
+                Text(text = "BackStack = ${backStackState.value.toStateString()}")
                 Row {
                     listOf("A", "B", "C", "D").forEach {
                         Row {
@@ -90,6 +95,14 @@ class BackStackExampleNode(
                     }
                 }
             }
+        }
+    }
+
+    private fun Elements<Routing>.toStateString() = map { element ->
+        (element.key as BackStack.LocalRoutingKey).let { key ->
+            val name = (key.routing as Child).name
+            val uuid = key.uuid
+            "$name(id: $uuid)"
         }
     }
 }
