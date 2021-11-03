8efc869a0f1a8781946d9544fec3a41e6e1be836
Gael Marhic <gael.marhic@team.bumble.com>
2021-11-03 10:20:08 +0100

    Add unit tests for NewRoot operation

--- library/src/test/java/com/github/zsoltk/composeribs/core/routing/source/backstack/operation/NewRootTest.kt
+++ library/src/test/java/com/github/zsoltk/composeribs/core/routing/source/backstack/operation/NewRootTest.kt
@@ -1,4 +1,103 @@
 package com.github.zsoltk.composeribs.core.routing.source.backstack.operation
 
+import com.github.zsoltk.composeribs.core.routing.source.backstack.BackStack.TransitionState.*
+import com.github.zsoltk.composeribs.core.routing.source.backstack.BackStackElement
+import com.github.zsoltk.composeribs.core.routing.source.backstack.UuidGenerator
+import com.github.zsoltk.composeribs.core.routing.source.backstack.operation.Routing.*
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
 internal class NewRootTest {
+
+    @Test
+    fun `is always applicable`() {
+
+        val elements = emptyList<BackStackElement<Routing>>()
+        val operation = NewRoot<Routing>(element = Routing1)
+
+        val applicable = operation.isApplicable(elements)
+
+        assertEquals(applicable, true)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `crashes when no element on screen`() {
+
+        val elements = emptyList<BackStackElement<Routing>>()
+        val operation = NewRoot<Routing>(element = Routing1)
+
+        operation.invoke(elements, UuidGenerator(0))
+    }
+
+    @Test
+    fun `passed element becomes the only one when currently on screen`() {
+
+        val elements = listOf<BackStackElement<Routing>>(
+            backStackElement(
+                element = Routing1,
+                uuid = 1,
+                fromState = STASHED_IN_BACK_STACK,
+                targetState = STASHED_IN_BACK_STACK
+            ),
+            backStackElement(
+                element = Routing2,
+                uuid = 2,
+                fromState = ON_SCREEN,
+                targetState = ON_SCREEN
+            )
+        )
+
+        val operation = NewRoot<Routing>(element = Routing2)
+
+        val newElements = operation.invoke(elements = elements, uuidGenerator = UuidGenerator(2))
+
+        val expectedElements = listOf<BackStackElement<Routing>>(
+            backStackElement(
+                element = Routing2,
+                uuid = 2,
+                fromState = ON_SCREEN,
+                targetState = ON_SCREEN
+            )
+        )
+        assertEquals(newElements, expectedElements)
+    }
+
+    @Test
+    fun `destroys current element on screen, remove the rest, and add on screen the newly created one`() {
+
+        val elements = listOf<BackStackElement<Routing>>(
+            backStackElement(
+                element = Routing1,
+                uuid = 1,
+                fromState = STASHED_IN_BACK_STACK,
+                targetState = STASHED_IN_BACK_STACK
+            ),
+            backStackElement(
+                element = Routing2,
+                uuid = 2,
+                fromState = ON_SCREEN,
+                targetState = ON_SCREEN
+            )
+        )
+
+        val operation = NewRoot<Routing>(element = Routing3)
+
+        val newElements = operation.invoke(elements = elements, uuidGenerator = UuidGenerator(2))
+
+        val expectedElements = listOf<BackStackElement<Routing>>(
+            backStackElement(
+                element = Routing2,
+                uuid = 2,
+                fromState = ON_SCREEN,
+                targetState = DESTROYED
+            ),
+            backStackElement(
+                element = Routing3,
+                uuid = 3,
+                fromState = CREATED,
+                targetState = ON_SCREEN
+            )
+        )
+        assertEquals(newElements, expectedElements)
+    }
 }
