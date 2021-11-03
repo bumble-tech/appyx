42c95c848764b512eaca13a6ce2ece0fc2b1365b
Gael Marhic <gael.marhic@team.bumble.com>
2021-11-03 10:19:17 +0100

    Add unit tests for Pop operation

--- library/src/test/java/com/github/zsoltk/composeribs/core/routing/source/backstack/operation/PopTest.kt
+++ library/src/test/java/com/github/zsoltk/composeribs/core/routing/source/backstack/operation/PopTest.kt
@@ -1,4 +1,110 @@
 package com.github.zsoltk.composeribs.core.routing.source.backstack.operation
 
+import com.github.zsoltk.composeribs.core.routing.source.backstack.BackStack.TransitionState.*
+import com.github.zsoltk.composeribs.core.routing.source.backstack.BackStackElement
+import com.github.zsoltk.composeribs.core.routing.source.backstack.UuidGenerator
+import com.github.zsoltk.composeribs.core.routing.source.backstack.operation.Routing.Routing1
+import com.github.zsoltk.composeribs.core.routing.source.backstack.operation.Routing.Routing2
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
 internal class PopTest {
+
+    @Test
+    fun `not applicable when no stashed element`() {
+
+        val elements = listOf<BackStackElement<Routing>>(
+            backStackElement(
+                element = Routing1,
+                uuid = 1,
+                fromState = ON_SCREEN,
+                targetState = ON_SCREEN
+            )
+        )
+        val operation = Pop<Routing>()
+
+        val applicable = operation.isApplicable(elements)
+
+        assertEquals(applicable, false)
+    }
+
+    @Test
+    fun `not applicable when no on screen element`() {
+
+        val elements = listOf<BackStackElement<Routing>>(
+            backStackElement(
+                element = Routing1,
+                uuid = 1,
+                fromState = STASHED_IN_BACK_STACK,
+                targetState = STASHED_IN_BACK_STACK
+            )
+        )
+        val operation = Pop<Routing>()
+
+        val applicable = operation.isApplicable(elements)
+
+        assertEquals(applicable, false)
+    }
+
+    @Test
+    fun `applicable when on screen and stashed elements present`() {
+
+        val elements = listOf<BackStackElement<Routing>>(
+            backStackElement(
+                element = Routing1,
+                uuid = 1,
+                fromState = STASHED_IN_BACK_STACK,
+                targetState = STASHED_IN_BACK_STACK
+            ),
+            backStackElement(
+                element = Routing2,
+                uuid = 2,
+                fromState = ON_SCREEN,
+                targetState = ON_SCREEN
+            ),
+        )
+        val operation = Pop<Routing>()
+
+        val applicable = operation.isApplicable(elements)
+
+        assertEquals(applicable, true)
+    }
+
+    @Test
+    fun `destroys current element on screen and add on screen last stashed element`() {
+
+        val elements = listOf<BackStackElement<Routing>>(
+            backStackElement(
+                element = Routing1,
+                uuid = 1,
+                fromState = STASHED_IN_BACK_STACK,
+                targetState = STASHED_IN_BACK_STACK
+            ),
+            backStackElement(
+                element = Routing2,
+                uuid = 2,
+                fromState = ON_SCREEN,
+                targetState = ON_SCREEN
+            )
+        )
+        val operation = Pop<Routing>()
+
+        val newElements = operation.invoke(elements = elements, uuidGenerator = UuidGenerator(0))
+
+        val expectedElements = listOf<BackStackElement<Routing>>(
+            backStackElement(
+                element = Routing1,
+                uuid = 1,
+                fromState = STASHED_IN_BACK_STACK,
+                targetState = ON_SCREEN
+            ),
+            backStackElement(
+                element = Routing2,
+                uuid = 2,
+                fromState = ON_SCREEN,
+                targetState = DESTROYED
+            )
+        )
+        assertEquals(newElements, expectedElements)
+    }
 }
