4dd7cc9236d2cef543ba36f76e31217fd64f0104
Gaël Marhic <gael.marhic.pro@gmail.com>
2021-11-08 13:49:10 +0100

    Make RoutingState

--- library/src/main/java/com/github/zsoltk/composeribs/core/routing/RoutingState.kt
+++ library/src/main/java/com/github/zsoltk/composeribs/core/routing/RoutingState.kt
@@ -1,6 +1,10 @@
 package com.github.zsoltk.composeribs.core.routing
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class RoutingState<Key, State>(
     val elements: RoutingElements<Key, State>,
     val operation: Operation<Key, State>
-)
+) : Parcelable
