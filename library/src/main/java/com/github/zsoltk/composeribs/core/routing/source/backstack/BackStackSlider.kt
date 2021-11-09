70a3c6ade18fd65995c96d94868c43a549fde4ee
Gaël Marhic <gael.marhic.pro@gmail.com>
2021-11-09 12:37:16 +0100

    Fix BackStack Destroy animations

--- library/src/main/java/com/github/zsoltk/composeribs/core/routing/source/backstack/BackStackSlider.kt
+++ library/src/main/java/com/github/zsoltk/composeribs/core/routing/source/backstack/BackStackSlider.kt
@@ -8,6 +8,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.unit.Dp
+import com.github.zsoltk.composeribs.core.routing.source.backstack.operation.NewRoot
+import com.github.zsoltk.composeribs.core.routing.source.backstack.operation.Pop
+import com.github.zsoltk.composeribs.core.routing.source.backstack.operation.Push
+import com.github.zsoltk.composeribs.core.routing.source.backstack.operation.Remove
+import com.github.zsoltk.composeribs.core.routing.source.backstack.operation.Replace
+import com.github.zsoltk.composeribs.core.routing.source.backstack.operation.SingleTop.SingleTopReactivateBackStackOperation
+import com.github.zsoltk.composeribs.core.routing.source.backstack.operation.SingleTop.SingleTopReplaceBackStackOperation
 import com.github.zsoltk.composeribs.core.routing.transition.TransitionDescriptor
 import com.github.zsoltk.composeribs.core.routing.transition.TransitionSpec
 import com.github.zsoltk.composeribs.core.routing.transition.UpdateTransitionHandler
@@ -28,15 +35,30 @@ class BackStackSlider<T>(
             targetValueByState = {
                 val width = descriptor.params.bounds.width.value
                 when (it) {
-                    BackStack.TransitionState.CREATED -> Offset(1.0f * width, 0f)
-                    BackStack.TransitionState.ON_SCREEN -> Offset(0f, 0f)
-                    BackStack.TransitionState.STASHED_IN_BACK_STACK -> Offset(-1.0f * width, 0f)
-                    BackStack.TransitionState.DESTROYED -> Offset(1.0f * width, 0f)
+                    BackStack.TransitionState.CREATED -> toOutsideRight(width)
+                    BackStack.TransitionState.ON_SCREEN -> toCenter()
+                    BackStack.TransitionState.STASHED_IN_BACK_STACK -> toOutsideLeft(width)
+                    BackStack.TransitionState.DESTROYED -> {
+                        when (val operation = descriptor.operation) {
+                            is Push,
+                            is Pop,
+                            is Remove,
+                            is SingleTopReactivateBackStackOperation -> toOutsideRight(width)
+                            is Replace,
+                            is NewRoot,
+                            is SingleTopReplaceBackStackOperation -> toOutsideLeft(width)
+                            else -> throw IllegalArgumentException("Unexpected operation: $operation")
+                        }
+                    }
                 }
             })
 
-
         return Modifier.offset(Dp(offset.value.x), Dp(offset.value.y))
-
     }
+
+    private fun toOutsideRight(width: Float) = Offset(1.0f * width, 0f)
+
+    private fun toOutsideLeft(width: Float) = Offset(-1.0f * width, 0f)
+
+    private fun toCenter() = Offset(0f, 0f)
 }
