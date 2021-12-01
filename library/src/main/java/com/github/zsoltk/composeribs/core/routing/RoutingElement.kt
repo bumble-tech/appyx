3965d145a2f41b7592edbee0518bceb3e633127c
Anton Shilov <vaccum121@gmail.com>
2021-12-01 10:19:55 +0300

    Make transition history constructor private

--- library/src/main/java/com/github/zsoltk/composeribs/core/routing/RoutingElement.kt
+++ library/src/main/java/com/github/zsoltk/composeribs/core/routing/RoutingElement.kt
@@ -1,52 +1,80 @@
 package com.github.zsoltk.composeribs.core.routing
 
 import android.os.Parcelable
-import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.parcelize.Parcelize
 import kotlinx.parcelize.RawValue
 
 @Parcelize
-data class RoutingElement<Key, State> internal constructor(
+class RoutingElement<Key, State> private constructor(
     val key: @RawValue RoutingKey<Key>,
     val fromState: @RawValue State,
     val targetState: @RawValue State,
     val operation: @RawValue Operation<Key, out State>,
-    private val _transitionHistory: MutableList<Pair<State, State>>
+    val transitionHistory: List<Pair<State, State>>
 ) : Parcelable {
-
-    @IgnoredOnParcel
-    val transitionHistory: List<Pair<State, State>> = _transitionHistory
-
     constructor(
         key: @RawValue RoutingKey<Key>,
         fromState: @RawValue State,
         targetState: @RawValue State,
         operation: @RawValue Operation<Key, out State>,
-    ) : this(key, fromState, targetState, operation, mutableListOf(fromState to targetState))
+    ) : this(
+        key,
+        fromState,
+        targetState,
+        operation,
+        if (fromState == targetState) emptyList() else listOf(fromState to targetState)
+    )
 
     fun transitionTo(
         targetState: @RawValue State,
         operation: @RawValue Operation<Key, out State>
-    ): RoutingElement<Key, State> {
-        if (this.fromState != targetState) {
-            _transitionHistory.add(this.fromState to targetState)
-        }
-        return RoutingElement(
+    ): RoutingElement<Key, State> =
+        RoutingElement(
             key = key,
             fromState = fromState,
             targetState = targetState,
             operation = operation,
-            _transitionHistory = _transitionHistory
+            transitionHistory =
+            if (fromState != targetState) {
+                transitionHistory + listOf(fromState to targetState)
+            } else transitionHistory
         )
-    }
 
-    fun onTransitionFinished(): RoutingElement<Key, State> {
-        return RoutingElement(
+    fun onTransitionFinished(): RoutingElement<Key, State> =
+        RoutingElement(
             key = key,
             fromState = targetState,
             targetState = targetState,
             operation = operation,
-            _transitionHistory = mutableListOf()
+            transitionHistory = emptyList()
         )
+
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as RoutingElement<*, *>
+
+        if (key != other.key) return false
+        if (fromState != other.fromState) return false
+        if (targetState != other.targetState) return false
+        if (operation != other.operation) return false
+        if (transitionHistory != other.transitionHistory) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = key.hashCode()
+        result = 31 * result + (fromState?.hashCode() ?: 0)
+        result = 31 * result + (targetState?.hashCode() ?: 0)
+        result = 31 * result + operation.hashCode()
+        result = 31 * result + transitionHistory.hashCode()
+        return result
+    }
+
+    override fun toString(): String {
+        return "RoutingElement(key=$key, fromState=$fromState, targetState=$targetState, operation=$operation, transitionHistory=$transitionHistory)"
     }
 }
