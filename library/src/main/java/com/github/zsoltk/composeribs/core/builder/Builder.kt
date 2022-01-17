b9c12092f8c0dccaba9347c7f9a1f406102a4c96
andreikovalev <hipaw@yandex.ru>
2022-01-17 19:14:31 +0000

    change order of params in Builder.kt

--- library/src/main/java/com/github/zsoltk/composeribs/core/builder/Builder.kt
+++ library/src/main/java/com/github/zsoltk/composeribs/core/builder/Builder.kt
@@ -4,5 +4,5 @@ import com.github.zsoltk.composeribs.core.modality.BuildContext
 import com.github.zsoltk.composeribs.core.node.Node
 
 abstract class Builder<P> {
-    abstract fun build(payload: P, buildContext: BuildContext): Node
+    abstract fun build(buildContext: BuildContext, payload: P): Node
 }
