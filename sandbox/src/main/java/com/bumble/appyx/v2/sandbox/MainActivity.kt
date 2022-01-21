e4ef13f0144b3d938092e1a89295100ce7a083f5
andreikovalev <hipaw@yandex.ru>
2022-01-21 17:11:20 +0000

    move sandbox code under sandbox directory

--- sandbox/src/main/java/com/bumble/appyx/v2/sandbox/MainActivity.kt
+++ sandbox/src/main/java/com/bumble/appyx/v2/sandbox/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.bumble.appyx.v2.app
+package com.bumble.appyx.v2.sandbox
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
@@ -7,25 +7,23 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.bumble.appyx.v2.app.node.child.GenericChildNode
-import com.bumble.appyx.v2.sandbox.ui.AppyxSampleAppTheme
 import com.bumble.appyx.v2.core.integration.NodeHost
 import com.bumble.appyx.v2.core.integrationpoint.RibActivity
 import com.bumble.appyx.v2.core.modality.BuildContext
+import com.bumble.appyx.v2.sandbox.client.container.ContainerNode
+import com.bumble.appyx.v2.sandbox.ui.AppyxSandboxTheme
 
 class MainActivity : RibActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AppyxSampleAppTheme {
+            AppyxSandboxTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     Column {
                         NodeHost(integrationPoint = integrationPoint) {
-                            GenericChildNode(
-                                buildContext = it,
-                            )
+                            ContainerNode(buildContext = it)
                         }
                     }
                 }
@@ -38,9 +36,9 @@ class MainActivity : RibActivity() {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    AppyxSampleAppTheme {
+    AppyxSandboxTheme {
         Column {
-            GenericChildNode(buildContext = BuildContext.root(null)).Compose()
+            ContainerNode(buildContext = BuildContext.root(null)).Compose()
         }
     }
 }
