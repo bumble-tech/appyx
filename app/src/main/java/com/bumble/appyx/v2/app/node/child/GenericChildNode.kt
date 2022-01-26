396220e232057abf6c696d62074995b56dfe45ad
andreikovalev <hipaw@yandex.ru>
2022-01-26 09:11:45 +0000

    Make generic child node increase count being off compisition

--- app/src/main/java/com/bumble/appyx/v2/app/node/child/GenericChildNode.kt
+++ app/src/main/java/com/bumble/appyx/v2/app/node/child/GenericChildNode.kt
@@ -9,26 +9,42 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.SaverScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.coroutineScope
 import com.bumble.appyx.v2.app.ui.atomic_tangerine
 import com.bumble.appyx.v2.app.ui.manatee
 import com.bumble.appyx.v2.app.ui.silver_sand
 import com.bumble.appyx.v2.app.ui.sizzling_red
-import com.bumble.appyx.v2.sandbox.ui.*
 import com.bumble.appyx.v2.core.integration.NodeHost
 import com.bumble.appyx.v2.core.integrationpoint.IntegrationPointStub
 import com.bumble.appyx.v2.core.modality.BuildContext
 import com.bumble.appyx.v2.core.modality.BuildContext.Companion.root
 import com.bumble.appyx.v2.core.node.Node
 import com.bumble.appyx.v2.core.state.SavedStateMap
+import com.bumble.appyx.v2.sandbox.ui.md_amber_500
+import com.bumble.appyx.v2.sandbox.ui.md_blue_500
+import com.bumble.appyx.v2.sandbox.ui.md_blue_grey_500
+import com.bumble.appyx.v2.sandbox.ui.md_cyan_500
+import com.bumble.appyx.v2.sandbox.ui.md_grey_500
+import com.bumble.appyx.v2.sandbox.ui.md_indigo_500
+import com.bumble.appyx.v2.sandbox.ui.md_light_blue_500
+import com.bumble.appyx.v2.sandbox.ui.md_light_green_500
+import com.bumble.appyx.v2.sandbox.ui.md_lime_500
+import com.bumble.appyx.v2.sandbox.ui.md_pink_500
+import com.bumble.appyx.v2.sandbox.ui.md_teal_500
 import kotlin.random.Random
+import kotlinx.coroutines.delay
 
 class GenericChildNode(
     buildContext: BuildContext
@@ -67,6 +83,15 @@ class GenericChildNode(
         Random.nextInt(colors.size)
     )
 
+    init {
+        lifecycle.coroutineScope.launchWhenCreated {
+            while (true) {
+                counter++
+                delay(1000)
+            }
+        }
+    }
+
     override fun onSaveInstanceState(scope: SaverScope): SavedStateMap =
         super.onSaveInstanceState(scope) + mapOf(
             KEY_ID to id,
@@ -87,7 +112,6 @@ class GenericChildNode(
                 )
                 .pointerInput(Unit) {
                     detectTapGestures(
-                        onTap = { counter++ },
                         onLongPress = { colorIndex = Random.nextInt(colors.size) },
                     )
                 }
