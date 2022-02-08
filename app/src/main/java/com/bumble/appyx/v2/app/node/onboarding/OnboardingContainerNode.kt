485fdf9700d4f1d24619269b4ca2954adb8f4dd4
samuelps <samuel.perezsarmiento@team.bumble.com>
2022-02-08 15:44:25 +0100

    Extract to its own class. Create Spotlight wrapper.

--- app/src/main/java/com/bumble/appyx/v2/app/node/onboarding/OnboardingContainerNode.kt
+++ app/src/main/java/com/bumble/appyx/v2/app/node/onboarding/OnboardingContainerNode.kt
@@ -2,14 +2,7 @@ package com.bumble.appyx.v2.app.node.onboarding
 
 import android.os.Parcelable
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.VisibilityThreshold
-import androidx.compose.animation.core.animateDp
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.spring
-import androidx.compose.animation.core.updateTransition
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,10 +11,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -29,21 +19,17 @@ import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.ExperimentalUnitApi
 import androidx.compose.ui.unit.dp
+import com.bumble.appyx.v2.app.composable.graph.SpotlightDotsIndicator
 import com.bumble.appyx.v2.app.node.onboarding.OnboardingContainerNode.Routing
 import com.bumble.appyx.v2.app.node.onboarding.screen.ApplicationTree
 import com.bumble.appyx.v2.app.node.onboarding.screen.IntroScreen
@@ -52,7 +38,6 @@ import com.bumble.appyx.v2.app.node.onboarding.screen.StatefulNode1
 import com.bumble.appyx.v2.app.node.onboarding.screen.StatefulNode2
 import com.bumble.appyx.v2.app.ui.AppyxSampleAppTheme
 import com.bumble.appyx.v2.app.ui.appyx_dark
-import com.bumble.appyx.v2.app.ui.appyx_yellow1
 import com.bumble.appyx.v2.core.composable.Children
 import com.bumble.appyx.v2.core.integration.NodeHost
 import com.bumble.appyx.v2.core.integrationpoint.IntegrationPointStub
@@ -61,8 +46,6 @@ import com.bumble.appyx.v2.core.modality.BuildContext.Companion.root
 import com.bumble.appyx.v2.core.node.Node
 import com.bumble.appyx.v2.core.node.ParentNode
 import com.bumble.appyx.v2.core.routing.source.spotlight.Spotlight
-import com.bumble.appyx.v2.core.routing.source.spotlight.activeIndex
-import com.bumble.appyx.v2.core.routing.source.spotlight.elementsCount
 import com.bumble.appyx.v2.core.routing.source.spotlight.hasNext
 import com.bumble.appyx.v2.core.routing.source.spotlight.hasPrevious
 import com.bumble.appyx.v2.core.routing.source.spotlight.operations.next
@@ -120,7 +103,6 @@ class OnboardingContainerNode(
     override fun View(modifier: Modifier) {
         val hasPrevious = spotlight.hasPrevious().collectAsState(initial = false)
         val hasNext = spotlight.hasNext().collectAsState(initial = false)
-        val activeItem = spotlight.activeIndex().collectAsState(initial = 0)
         val previousVisibility = animateFloatAsState(
             targetValue = if (hasPrevious.value) 1f else 0f
         )
@@ -141,11 +123,8 @@ class OnboardingContainerNode(
                     .padding(bottom = 12.dp),
                 contentAlignment = Alignment.Center
             ) {
-                DotsIndicator(
-                    totalDots = spotlight.elementsCount(),
-                    selectedIndex = activeItem.value,
-                    selectedColor = appyx_yellow1,
-                    unSelectedColor = Color.DarkGray,
+                SpotlightDotsIndicator(
+                    spotlight = spotlight
                 )
                 Row(
                     modifier = Modifier
@@ -195,59 +174,6 @@ class OnboardingContainerNode(
         }
     }
 }
-
-
-@Composable
-private fun DotsIndicator(
-    totalDots: Int,
-    selectedIndex: Int,
-    selectedColor: Color,
-    unSelectedColor: Color,
-) {
-
-    LazyRow(
-        modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentHeight(),
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-
-        items(totalDots) { index ->
-            val isSelected = remember(index, selectedIndex) {
-                mutableStateOf(index == selectedIndex)
-            }
-            val transition = updateTransition(isSelected)
-            val color = transition.animateColor { isSelected ->
-                when (isSelected.value) {
-                    true -> selectedColor
-                    false -> unSelectedColor
-                }
-            }
-            val size = transition.animateDp(
-                transitionSpec = {
-                    spring(
-                        visibilityThreshold = Dp.VisibilityThreshold,
-                        dampingRatio = Spring.DampingRatioMediumBouncy,
-                        stiffness = Spring.StiffnessMediumLow
-                    )
-                }) { isSelected ->
-                when (isSelected.value) {
-                    true -> 9.dp
-                    false -> 6.dp
-                }
-            }
-            Box(
-                modifier = Modifier
-                    .size(size.value)
-                    .clip(CircleShape)
-                    .background(color.value)
-            )
-            if (index != totalDots - 1) Spacer(modifier = Modifier.padding(end = 5.dp))
-        }
-    }
-}
-
 
 @Preview
 @Composable
