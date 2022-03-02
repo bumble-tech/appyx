ebc8094218f29c764b8cfedb95f1469e241387ff
andreikovalev <hipaw@yandex.ru>
2022-03-02 13:06:11 +0000

    remove initialised logic from asbtract view

--- library/src/main/java/com/bumble/appyx/v2/core/node/AbstractNodeView.kt
+++ library/src/main/java/com/bumble/appyx/v2/core/node/AbstractNodeView.kt
@@ -1,32 +1,21 @@
 package com.bumble.appyx.v2.core.node
 
-import android.util.Log
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.bumble.appyx.v2.core.plugin.NodeAware
 
 abstract class AbstractNodeView<N : Node> : NodeView, NodeAware<N> {
-
-    private var initialised: Boolean by mutableStateOf(false)
 
     final override lateinit var node: N
         private set
 
     override fun init(node: N) {
         this.node = node
-        initialised = true
     }
 
     @Composable
     override fun View(modifier: Modifier) {
-        if (initialised) {
-            node.NodeView(modifier = modifier)
-        } else {
-            Log.e("AbstractNodeView", "$this not initialised yet")
-        }
+        node.NodeView(modifier = modifier)
     }
 
     @Composable
