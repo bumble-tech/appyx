e7110e5ed0aef4a5305a8908a26bf83c46764016
CherryPerry <ya.cherryperry@yandex.ru>
2022-05-11 16:51:22 +0100

    Remove BackStack.routings

--- library/src/main/java/com/bumble/appyx/v2/core/routing/source/backstack/BackStack.kt
+++ library/src/main/java/com/bumble/appyx/v2/core/routing/source/backstack/BackStack.kt
@@ -2,20 +2,16 @@ package com.bumble.appyx.v2.core.routing.source.backstack
 
 import com.bumble.appyx.v2.core.node.ParentNode
 import com.bumble.appyx.v2.core.routing.BaseRoutingSource
-import com.bumble.appyx.v2.core.routing.onscreen.OnScreenStateResolver
 import com.bumble.appyx.v2.core.routing.Operation
 import com.bumble.appyx.v2.core.routing.RoutingKey
 import com.bumble.appyx.v2.core.routing.backpresshandlerstrategies.BackPressHandlerStrategy
+import com.bumble.appyx.v2.core.routing.onscreen.OnScreenStateResolver
 import com.bumble.appyx.v2.core.routing.operationstrategies.ExecuteImmediately
 import com.bumble.appyx.v2.core.routing.operationstrategies.OperationStrategy
 import com.bumble.appyx.v2.core.routing.source.backstack.BackStack.TransitionState
 import com.bumble.appyx.v2.core.routing.source.backstack.BackStack.TransitionState.DESTROYED
 import com.bumble.appyx.v2.core.routing.source.backstack.backpresshandler.PopBackPressHandler
 import com.bumble.appyx.v2.core.state.SavedStateMap
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
 
 class BackStack<Routing : Any>(
     initialElement: Routing,
@@ -45,11 +41,5 @@ class BackStack<Routing : Any>(
             operation = Operation.Noop()
         )
     )
-
-    // TODO consider pulling up
-    val routings: StateFlow<List<Routing>> =
-        elements
-            .map { state -> state.map { routingElement -> routingElement.key.routing } }
-            .stateIn(scope, SharingStarted.Eagerly, listOf(initialElement))
 
 }
