82e4c0715ba217aba7b3e921055b157a8eb43e74
CherryPerry <ya.cherryperry@yandex.ru>
2022-05-30 11:11:56 +0100

    Restore key collision check

--- library/src/main/java/com/bumble/appyx/v2/core/state/MutableSavedStateMapImpl.kt
+++ library/src/main/java/com/bumble/appyx/v2/core/state/MutableSavedStateMapImpl.kt
@@ -15,16 +15,26 @@ class MutableSavedStateMapImpl(
 
     override fun put(key: String, value: Any?): Any? {
         checkState()
+        checkKey(key, value)
         return map.put(key, value)
     }
 
     override fun putAll(from: Map<out String, Any?>) {
         checkState()
+        from.entries.forEach { checkKey(it.key, it.value) }
         return map.putAll(from)
     }
 
     private fun checkState() {
         check(!lock) { "This MutableSavedStateMap has already dumped its state, it is meaningless to write anything anymore" }
+    }
+
+    private fun checkKey(key: String, value: Any?) {
+        check(!map.containsKey(key)) {
+            "Save instance process has faced key collision at '${key}': " +
+                    "existing value is '${map[key]}', " +
+                    "new value is '$value'"
+        }
     }
 
 }
--- library/src/test/java/com/bumble/appyx/v2/core/plugin/SavesInstanceStateTest.kt
+++ library/src/test/java/com/bumble/appyx/v2/core/plugin/SavesInstanceStateTest.kt
@@ -8,6 +8,7 @@ import com.bumble.appyx.v2.core.node.Node
 import com.bumble.appyx.v2.core.node.build
 import com.bumble.appyx.v2.core.state.MutableSavedStateMap
 import com.bumble.appyx.v2.core.testutils.MainDispatcherRule
+import org.junit.Assert.assertThrows
 import org.junit.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
@@ -22,11 +23,7 @@ class SavesInstanceStateTest {
 
     @Test
     fun `saveInstanceState is invoked when node saves instance state`() {
-        val stub = object : SavesInstanceState {
-            override fun saveInstanceState(state: MutableSavedStateMap) {
-                state["key"] = "value"
-            }
-        }
+        val stub = createPlugin()
 
         val node = createNode(stub)
         node.build()
@@ -34,6 +31,26 @@ class SavesInstanceStateTest {
 
         assertTrue(state["key"] == "value")
     }
+
+    @Test
+    fun `saveInstanceState does not allow duplicate keys`() {
+        val stub1 = createPlugin()
+        val stub2 = createPlugin()
+
+        val node = createNode(stub1, stub2)
+        node.build()
+
+        assertThrows(IllegalStateException::class.java) {
+            node.saveInstanceState { true }
+        }
+    }
+
+    private fun createPlugin() =
+        object : SavesInstanceState {
+            override fun saveInstanceState(state: MutableSavedStateMap) {
+                state["key"] = "value"
+            }
+        }
 
     private fun createNode(vararg plugins: Plugin) =
         object : Node(BuildContext.root(null), plugins = plugins.toList()) {
