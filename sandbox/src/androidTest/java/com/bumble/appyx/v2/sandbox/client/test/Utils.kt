ffff8af5a09ccc20d3ce6718537a5bf5fbe88ca7
andreikovalev <andrey.kovalev@team.bumble.com>
2022-06-13 11:23:40 +0100

    Remove redundant assertJ dependency

--- sandbox/src/androidTest/java/com/bumble/appyx/v2/sandbox/client/test/Utils.kt
+++ sandbox/src/androidTest/java/com/bumble/appyx/v2/sandbox/client/test/Utils.kt
@@ -4,8 +4,9 @@ import androidx.test.platform.app.InstrumentationRegistry
 import io.reactivex.Observable
 import io.reactivex.ObservableSource
 import io.reactivex.observers.TestObserver
-import org.assertj.core.api.Assertions.assertThat
 import java.util.concurrent.atomic.AtomicReference
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.greaterThan
 
 fun <T> runOnMainSync(block: () -> T): T {
     val result = AtomicReference<T>()
@@ -21,6 +22,6 @@ fun <T> ObservableSource<out T>.wrapToObservable(): Observable<T> = Observable.w
 inline fun <reified T> Any?.cast(): T = this as T
 
 fun <T> TestObserver<T>.assertLastValueEqual(value: T) {
-    assertThat(this.valueCount()).isGreaterThan(0)
+    assertThat(this.valueCount(), greaterThan(0))
     this.assertValueAt(valueCount() - 1, value)
 }
