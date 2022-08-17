92602ebf03df936a09897531b9b76429d6f616bb
andreykovalev <andrey.kovalev@team.bumble.com>
2022-08-17 10:37:52 +0100

    Attach integrationPoint at the moment of creation to prevent it from crashing

--- core/src/main/java/com/bumble/appyx/core/integration/NodeHost.kt
+++ core/src/main/java/com/bumble/appyx/core/integration/NodeHost.kt
@@ -2,7 +2,6 @@ package com.bumble.appyx.core.integration
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,6 +16,7 @@ import com.bumble.appyx.core.integrationpoint.IntegrationPoint
 import com.bumble.appyx.core.modality.BuildContext
 import com.bumble.appyx.core.node.Node
 import com.bumble.appyx.core.node.build
+import com.bumble.appyx.core.state.SavedStateMap
 
 /**
  * Composable function to host [Node].
@@ -29,10 +29,7 @@ fun <N : Node> NodeHost(
     integrationPoint: IntegrationPoint,
     factory: NodeFactory<N>
 ) {
-    val node by rememberNode(factory, customisations)
-    LaunchedEffect(integrationPoint, node) {
-        node.integrationPoint = integrationPoint
-    }
+    val node by rememberNode(factory, customisations, integrationPoint)
     DisposableEffect(node) {
         onDispose { node.updateLifecycleState(Lifecycle.State.DESTROYED) }
     }
@@ -49,30 +46,30 @@ fun <N : Node> NodeHost(
 }
 
 @Composable
-internal fun <N : Node> rememberNode(
+private fun <N : Node> rememberNode(
     factory: NodeFactory<N>,
-    customisations: NodeCustomisationDirectory
-): State<N> =
-    rememberSaveable(
+    customisations: NodeCustomisationDirectory,
+    integrationPoint: IntegrationPoint,
+): State<N> {
+
+    fun createNode(savedStateMap: SavedStateMap?): N =
+        factory
+            .create(
+                buildContext = BuildContext.root(
+                    savedStateMap = savedStateMap,
+                    customisations = customisations
+                ),
+            )
+            .build()
+            .apply { this.integrationPoint = integrationPoint }
+
+    return rememberSaveable(
         inputs = arrayOf(),
         stateSaver = mapSaver(
             save = { node -> node.saveInstanceState(this) },
-            restore = { state ->
-                factory.create(
-                    buildContext = BuildContext.root(
-                        savedStateMap = state,
-                        customisations = customisations
-                    ),
-                ).build()
-            },
+            restore = { state -> createNode(savedStateMap = state) },
         ),
     ) {
-        mutableStateOf(
-            factory.create(
-                buildContext = BuildContext.root(
-                    savedStateMap = null,
-                    customisations = customisations
-                )
-            ).build()
-        )
+        mutableStateOf(createNode(savedStateMap = null))
     }
+}
