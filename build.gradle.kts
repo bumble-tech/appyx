52e4b998a76bc26171b32270e3153a614d27f589
Lachlan McKee <lachlan.mckee@team.bumble.com>
2022-08-18 22:25:21 +0100

    Migrated gradle files to kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+    }
+    dependencies {
+        classpath(libs.plugin.android)
+        classpath(libs.plugin.kotlin)
+    }
+}
+
+plugins {
+    id("appyx-collect-sarif")
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
+
+allprojects {
+    configurations.all {
+        resolutionStrategy.dependencySubstitution {
+            substitute(module("com.bumble.appyx:customisations"))
+                .using(project(":customisations"))
+                .because("RIBs uses Appyx customisations as external dependency")
+        }
+    }
+}
