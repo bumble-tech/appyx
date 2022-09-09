6dfafbcea8d96455f6f017932713898d849f4f22
andreykovalev <andrey.kovalev@team.bumble.com>
2022-09-09 12:26:15 +0100

    Make NodeView.kt interface stable

--- core/src/main/kotlin/com/bumble/appyx/core/node/NodeView.kt
+++ core/src/main/kotlin/com/bumble/appyx/core/node/NodeView.kt
@@ -1,8 +1,10 @@
 package com.bumble.appyx.core.node
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.Modifier
 
+@Stable
 interface NodeView {
 
     @Composable
