1d6dee32a1737ec1455f3c891950f8f4f18b1494
Zsolt Kocsi <zsolt.kocsi@team.bumble.com>
2022-10-09 18:09:35 +0100

    Add Spotlight operations First and Last

---
+++ libraries/core/src/main/kotlin/com/bumble/appyx/navmodel2/spotlight/operation/First.kt
@@ -0,0 +1,40 @@
+package com.bumble.appyx.navmodel2.spotlight.operation
+
+import com.bumble.appyx.core.navigation2.NavElements
+import com.bumble.appyx.core.navigation2.NavTransition
+import com.bumble.appyx.core.navigation2.Operation
+import com.bumble.appyx.navmodel2.spotlight.Spotlight
+import com.bumble.appyx.navmodel2.spotlight.Spotlight.State
+import com.bumble.appyx.navmodel2.spotlight.Spotlight.State.ACTIVE
+import com.bumble.appyx.navmodel2.spotlight.Spotlight.State.INACTIVE_AFTER
+import com.bumble.appyx.navmodel2.spotlight.Spotlight.State.INACTIVE_BEFORE
+import kotlinx.parcelize.Parcelize
+
+
+@Parcelize
+class First<NavTarget : Any> : Operation<NavTarget, State> {
+
+    override fun isApplicable(elements: NavElements<NavTarget, State>) =
+        elements.any { it.state == INACTIVE_BEFORE }
+
+    override fun invoke(elements: NavElements<NavTarget, State>): NavTransition<NavTarget, State> {
+        return NavTransition(
+            fromState = elements,
+            targetState = elements.mapIndexed { index, element ->
+                element.transitionTo(
+                    newTargetState = when (index) {
+                        0 -> ACTIVE
+                        else -> INACTIVE_AFTER
+                    },
+                    operation = this
+                )
+            }
+        )
+    }
+}
+
+fun <T : Any> Spotlight<T>.first() {
+    enqueue(First())
+}
+
+
---
+++ libraries/core/src/main/kotlin/com/bumble/appyx/navmodel2/spotlight/operation/Last.kt
@@ -0,0 +1,40 @@
+package com.bumble.appyx.navmodel2.spotlight.operation
+
+import com.bumble.appyx.core.navigation2.NavElements
+import com.bumble.appyx.core.navigation2.NavTransition
+import com.bumble.appyx.core.navigation2.Operation
+import com.bumble.appyx.navmodel2.spotlight.Spotlight
+import com.bumble.appyx.navmodel2.spotlight.Spotlight.State
+import com.bumble.appyx.navmodel2.spotlight.Spotlight.State.ACTIVE
+import com.bumble.appyx.navmodel2.spotlight.Spotlight.State.INACTIVE_AFTER
+import com.bumble.appyx.navmodel2.spotlight.Spotlight.State.INACTIVE_BEFORE
+import kotlinx.parcelize.Parcelize
+
+
+@Parcelize
+class Last<NavTarget : Any> : Operation<NavTarget, State> {
+
+    override fun isApplicable(elements: NavElements<NavTarget, State>) =
+        elements.any { it.state == INACTIVE_AFTER }
+
+    override fun invoke(elements: NavElements<NavTarget, State>): NavTransition<NavTarget, State> {
+        return NavTransition(
+            fromState = elements,
+            targetState = elements.mapIndexed { index, element ->
+                element.transitionTo(
+                    newTargetState = when (index) {
+                        elements.lastIndex -> ACTIVE
+                        else -> INACTIVE_BEFORE
+                    },
+                    operation = this
+                )
+            }
+        )
+    }
+}
+
+fun <T : Any> Spotlight<T>.last() {
+    enqueue(Last())
+}
+
+
