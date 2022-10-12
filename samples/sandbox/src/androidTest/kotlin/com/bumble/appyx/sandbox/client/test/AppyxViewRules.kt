58c03b6bb31cb525c254ec0f3e3d40f90db7fb0d
andreykovalev <andrey.kovalev@team.bumble.com>
2022-10-12 16:39:53 +0100

    Always explicitly start test activity

--- samples/sandbox/src/androidTest/kotlin/com/bumble/appyx/sandbox/client/test/AppyxViewRules.kt
+++ samples/sandbox/src/androidTest/kotlin/com/bumble/appyx/sandbox/client/test/AppyxViewRules.kt
@@ -6,11 +6,9 @@ import io.reactivex.ObservableSource
 import io.reactivex.functions.Consumer
 
 fun <ViewModel : Any, Event : Any, View> appyxViewRule(
-    launchActivity: Boolean = true,
     viewFactory: ViewFactory<View>,
 ) where View : NodeView, View : Consumer<in ViewModel>, View : ObservableSource<out Event> =
     AppyxMviViewTestRule(
-        launchActivity = launchActivity,
         modelConsumer = { it },
         eventObservable = { it },
         viewFactory = viewFactory
