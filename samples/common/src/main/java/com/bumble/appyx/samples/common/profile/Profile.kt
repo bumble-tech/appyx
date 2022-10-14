d43c11cb2dd0b2cb6a87800313930fe8c2b7075f
Zsolt Kocsi <zsolt.kocsi@team.bumble.com>
2022-10-14 14:04:41 +0100

    Replace images with halloween-themed ones

--- samples/common/src/main/java/com/bumble/appyx/samples/common/profile/Profile.kt
+++ samples/common/src/main/java/com/bumble/appyx/samples/common/profile/Profile.kt
@@ -13,80 +13,73 @@ data class Profile(
     val age: Int = Random.nextInt(20, 35),
     val city: String = "London",
     val uni: String = "Oxford University",
-    @DrawableRes val drawable: Int = drawables.random()
+    @DrawableRes val drawable: Int = allProfiles.random().drawable
 ) : Parcelable {
 
     companion object {
-        val profile001 = Profile(
-            name = "Eve",
-            drawable = R.drawable.img_8607
-        )
-
         val profile002 = Profile(
             name = "Victoria",
-            drawable = R.drawable.img_9113
+            drawable = R.drawable.halloween3
         )
 
         val profile1001 = Profile(
             name = "Brittany",
-            drawable = R.drawable.brittany8
-        )
-
-        val profile1002 = Profile(
-            name = "Zoe",
-            drawable = R.drawable.brittany41
+            drawable = R.drawable.halloween4
         )
 
         val profile1003 = Profile(
             name = "Jill",
-            drawable = R.drawable.brittany60
+            drawable = R.drawable.halloween6
         )
 
         val profile2001 = Profile(
             name = "Matt",
-            drawable = R.drawable.matt12
+            drawable = R.drawable.halloween7
         )
 
         val profile2002 = Profile(
             name = "Ryan",
-            drawable = R.drawable.matt24
+            drawable = R.drawable.halloween8
         )
 
         val profile2003 = Profile(
             name = "Chris",
-            drawable = R.drawable.matt60
+            drawable = R.drawable.halloween9
         )
 
         val profile2004 = Profile(
             name = "Daniel",
-            drawable = R.drawable.matt88
+            drawable = R.drawable.halloween10
         )
 
         val profile3001 = Profile(
             name = "Imogen",
-            drawable = R.drawable.imogen15
+            drawable = R.drawable.halloween11
         )
 
         val profile3002 = Profile(
             name = "Heather",
-            drawable = R.drawable.imogen57
+            drawable = R.drawable.halloween12
         )
 
         val profile3003 = Profile(
             name = "Maria",
-            drawable = R.drawable.imogen60
+            drawable = R.drawable.halloween13
         )
 
         val profile3004 = Profile(
             name = "Sophia",
-            drawable = R.drawable.imogen75
+            drawable = R.drawable.halloween14
+        )
+
+        val profile3005 = Profile(
+            name = "Sophia",
+            drawable = R.drawable.halloween15
         )
 
         val allProfiles = listOf(
-            profile001,
             profile002,
             profile1001,
-            profile1002,
             profile1003,
             profile2001,
             profile2002,
@@ -96,22 +89,8 @@ data class Profile(
             profile3002,
             profile3003,
             profile3004,
+            profile3005,
         )
     }
 }
 
-private val drawables: List<Int> = listOf(
-    R.drawable.img_8607,
-    R.drawable.img_9113,
-    R.drawable.brittany8,
-    R.drawable.brittany41,
-    R.drawable.brittany60,
-    R.drawable.matt12,
-    R.drawable.matt24,
-    R.drawable.matt60,
-    R.drawable.matt88,
-    R.drawable.imogen15,
-    R.drawable.imogen57,
-    R.drawable.imogen60,
-    R.drawable.imogen75
-)
