93301abb42bf6d0bfe62b1c9ca905172c4e3b2e1
Zsolt Kocsi <zsolt.kocsi@team.bumble.com>
2022-10-17 13:01:37 +0100

    Remove unused dependencies

--- libraries/core/build.gradle.kts
+++ libraries/core/build.gradle.kts
@@ -37,7 +37,6 @@ dependencies {
     api(libs.androidx.lifecycle.common)
     api(libs.compose.animation.core)
     api(libs.compose.runtime)
-    api(libs.compose.material)
     api(libs.compose.ui.tooling)
     api(libs.compose.ui.ui)
     api(libs.androidx.appcompat)
