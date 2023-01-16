0f102846170e33d59fdf0316c167a5a0f8524b03
Zsolt Kocsi <zsolt.kocsi@team.bumble.com>
2023-01-16 14:01:58 +0000

    Remove all dependencies for now

--- appyx-interactions/build.gradle.kts
+++ appyx-interactions/build.gradle.kts
@@ -22,20 +22,5 @@ android {
 }
 
 dependencies {
-    val composeBom = platform(libs.compose.bom)
-
-    api(composeBom)
-    api(libs.kotlin.coroutines.android)
-    api(libs.compose.animation.core)
-    api(libs.compose.runtime)
-    api(libs.compose.foundation)
-    api(libs.compose.ui.tooling)
-    api(libs.compose.ui.ui)
-
-    implementation(composeBom)
-    implementation(libs.compose.foundation.layout)
-
-    testImplementation(project(":libraries:testing-junit4"))
-    testImplementation(libs.junit)
-    testImplementation(libs.kotlin.coroutines.test)
+    
 }
