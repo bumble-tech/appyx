d049735e3d506095711d4d65fb09c99a6967dd82
andreykovalev <andrey.kovalev@team.bumble.com>
2023-01-16 11:19:12 +0000

    fix material3 dependency

--- samples/sandbox/build.gradle.kts
+++ samples/sandbox/build.gradle.kts
@@ -62,7 +62,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.lifecycle.java8)
     implementation(libs.androidx.lifecycle.runtime)
-    implementation(libs.compose.material)
+    implementation(libs.compose.material3)
     implementation(libs.compose.ui.tooling)
     implementation(libs.compose.ui.ui)
     implementation(libs.google.accompanist.flow)
