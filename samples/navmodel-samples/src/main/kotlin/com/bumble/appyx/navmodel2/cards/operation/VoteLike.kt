4bdb966cd337733294635b8277ec03d0689d3cae
andreykovalev <andrey.kovalev@team.bumble.com>
2023-01-17 16:05:46 +0000

    remove mavmodel2 old samples

--- samples/navmodel-samples/src/main/kotlin/com/bumble/appyx/navmodel2/cards/operation/VoteLike.kt
+++
@@ -1,7 +0,0 @@
-package com.bumble.appyx.navmodel2.cards.operation
-
-import com.bumble.appyx.navmodel2.cards.Cards
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-class VoteLike<NavTarget : Any> : TopCardOperation<NavTarget>(Cards.State.VoteLike)
