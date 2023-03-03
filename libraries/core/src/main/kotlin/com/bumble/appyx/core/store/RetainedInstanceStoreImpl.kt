2ed1f11d694688ea3338814d4fc9e9d2eaff9498
Lachlan McKee <lachlan.mckee@team.bumble.com>
2023-03-03 16:30:44 +0000

    Added mechanism to query retained instance store

--- libraries/core/src/main/kotlin/com/bumble/appyx/core/store/RetainedInstanceStoreImpl.kt
+++ libraries/core/src/main/kotlin/com/bumble/appyx/core/store/RetainedInstanceStoreImpl.kt
@@ -5,15 +5,29 @@ internal class RetainedInstanceStoreImpl : RetainedInstanceStore {
     private val map: MutableMap<String, MutableMap<String, ValueHolder<*>>> = HashMap()
 
     @Suppress("UNCHECKED_CAST")
-    override fun <T : Any> get(storeId: String, key: String, disposer: (T) -> Unit, factory: () -> T): T =
+    override fun <T : Any> get(
+        storeId: String,
+        key: String,
+        disposer: (T) -> Unit,
+        factory: () -> T
+    ): T =
         map
             .getOrPut(storeId) { HashMap() }
             .getOrPut(key) { ValueHolder(factory(), disposer) }
             .value as T
 
+    override fun isRetainedByStoreId(storeId: String, value: Any): Boolean =
+        map[storeId]?.hasValue(value) ?: false
+
+    override fun isRetained(value: Any): Boolean =
+        map.values.any { valueHolderMap -> valueHolderMap.hasValue(value) }
+
     override fun clearStore(storeId: String) {
         map.remove(storeId)?.values?.forEach { it.dispose() }
     }
+
+    private fun MutableMap<String, ValueHolder<*>>.hasValue(value: Any): Boolean =
+        values.any { valueHolder -> valueHolder.value === value }
 
     private class ValueHolder<T : Any>(
         val value: T,
--- libraries/core/src/test/kotlin/com/bumble/appyx/core/store/RetainedInstanceStoreTest.kt
+++ libraries/core/src/test/kotlin/com/bumble/appyx/core/store/RetainedInstanceStoreTest.kt
@@ -81,4 +81,97 @@ class RetainedInstanceStoreTest {
 
         assertFalse(disposed)
     }
+
+    @Test
+    fun `GIVEN object stored before WHEN checking if same instance retained by store id with same owner THEN expect true`() {
+        val obj = Any()
+        store.get(identifier) { obj }
+
+        val isRetained = store.isRetainedByStoreId(identifier, obj)
+
+        assertTrue(isRetained)
+    }
+
+    @Test
+    fun `GIVEN object stored before WHEN checking if same instance retained by store id with different owner THEN expect false`() {
+        val obj = Any()
+        store.get(identifier) { obj }
+
+        val isRetained = store.isRetainedByStoreId("different-identifier", obj)
+
+        assertFalse(isRetained)
+    }
+
+    @Test
+    fun `GIVEN object stored before WHEN checking if different instance retained by store id with same owner THEN expect false`() {
+        val obj = Any()
+        val otherObj = Any()
+        store.get(identifier) { obj }
+
+        val isRetained = store.isRetainedByStoreId(identifier, otherObj)
+
+        assertFalse(isRetained)
+    }
+
+    @Test
+    fun `GIVEN object stored before AND then removed WHEN checking if same instance retained by store id with same owner THEN expect false`() {
+        val obj = Any()
+        store.get(identifier) { obj }
+        store.clearStore(identifier)
+
+        val isRetained = store.isRetainedByStoreId(identifier, obj)
+
+        assertFalse(isRetained)
+    }
+
+    @Test
+    fun `GIVEN no object stored WHEN checking if instance retained by store id THEN expect false`() {
+        val obj = Any()
+
+        val isRetained = store.isRetainedByStoreId(identifier, obj)
+
+        assertFalse(isRetained)
+    }
+
+    @Test
+    fun `GIVEN object stored before WHEN checking if same instance retained THEN expect true`() {
+        val obj = Any()
+        store.get(identifier) { obj }
+
+        val isRetained = store.isRetained(obj)
+
+        assertTrue(isRetained)
+    }
+
+    @Test
+    fun `GIVEN object stored before WHEN checking if different instance retained THEN expect false`() {
+        val obj = Any()
+        val otherObj = Any()
+        store.get(identifier) { obj }
+
+        val isRetained = store.isRetained(otherObj)
+
+        assertFalse(isRetained)
+    }
+
+    @Test
+    fun `GIVEN object stored before AND then removed WHEN checking if same instance retained THEN expect false`() {
+        val obj = Any()
+        store.get(identifier) { obj }
+        store.clearStore(identifier)
+
+        val isRetained = store.isRetained(obj)
+
+        assertFalse(isRetained)
+    }
+
+    @Test
+    fun `GIVEN no object stored WHEN checking if instance retained THEN expect false`() {
+        val obj = Any()
+
+        val isRetained = store.isRetained(obj)
+
+        assertFalse(isRetained)
+    }
+
 }
