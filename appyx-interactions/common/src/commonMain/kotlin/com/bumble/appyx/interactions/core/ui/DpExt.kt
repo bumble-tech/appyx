2c86dbc288be76d47818c75385d1d8225a64524a
andreykovalev <andrey.kovalev@team.bumble.com>
2023-03-17 10:08:38 +0000

    Fix utility function

--- appyx-interactions/common/src/commonMain/kotlin/com/bumble/appyx/interactions/core/ui/DpExt.kt
+++ appyx-interactions/common/src/commonMain/kotlin/com/bumble/appyx/interactions/core/ui/DpExt.kt
@@ -2,7 +2,5 @@ package com.bumble.appyx.interactions.core.ui
 
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.Dp
-import kotlin.math.roundToInt
 
-
-fun Dp.toPx(density: Density) = (value * density.density).roundToInt()
+internal fun Dp.toPx(density: Density) = with(density) { roundToPx() }
