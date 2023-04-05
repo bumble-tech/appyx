8cc3feb4a63d5c2196b3219155dc7dec6995e39c
vladcipariu <vladcipariu@gmail.com>
2023-04-05 13:47:46 +0100

    Re-enable KeyframeToUpdateTest.

--- appyx-interactions/android/src/androidTest/kotlin/com/bumble/appyx/interactions/sample/testdrive/KeyframesToUpdateTest.kt
+++ appyx-interactions/android/src/androidTest/kotlin/com/bumble/appyx/interactions/sample/testdrive/KeyframesToUpdateTest.kt
@@ -1,11 +1,11 @@
 package com.bumble.appyx.interactions.sample.testdrive
 
+import androidx.compose.animation.core.spring
 import androidx.compose.ui.test.junit4.createComposeRule
 import com.bumble.appyx.interactions.core.model.transition.Operation
 import com.bumble.appyx.interactions.sample.snapshot
 import com.bumble.appyx.interactions.sample.testdrive.helper.createTestDrive
 import com.bumble.appyx.transitionmodel.testdrive.operation.Next
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestName
@@ -18,23 +18,30 @@ class KeyframesToUpdateTest {
     @get:Rule
     var nameRule = TestName()
 
-    @Ignore("Fix me, this test is flaky on CI")
+    //    @Ignore("Fix me, this test is flaky on CI")
     @Test
     fun when_in_segment_interrupt_with_update() {
-        val testDrive = composeTestRule.createTestDrive(animationSpec = null)
+        val animationSpec = spring<Float>()
+        val testDrive = composeTestRule.createTestDrive(animationSpec = animationSpec)
         composeTestRule.mainClock.autoAdvance = false
 
         testDrive.operation(
-            operation = Next(Operation.Mode.KEYFRAME)
+            operation = Next(Operation.Mode.KEYFRAME),
+            animationSpec = animationSpec
         )
 
-        composeTestRule.mainClock.advanceTimeBy(100)
+        repeat(3) {
+            composeTestRule.mainClock.advanceTimeByFrame()
+        }
 
         testDrive.operation(
-            operation = Next(Operation.Mode.IMMEDIATE)
+            operation = Next(Operation.Mode.IMMEDIATE),
+            animationSpec = animationSpec
         )
 
-        composeTestRule.mainClock.advanceTimeBy(100)
+        repeat(3) {
+            composeTestRule.mainClock.advanceTimeByFrame()
+        }
 
         composeTestRule.snapshot("${javaClass.simpleName}_${nameRule.methodName}")
     }
