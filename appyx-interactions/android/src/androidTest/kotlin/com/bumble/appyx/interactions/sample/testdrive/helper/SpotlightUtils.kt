139f9a9a47bd50821ecd1ed34239c625e82083f7
Zsolt Kocsi <zsolt.kocsi@team.bumble.com>
2023-04-11 17:51:02 +0100

    Fix import

--- appyx-interactions/android/src/androidTest/kotlin/com/bumble/appyx/interactions/sample/testdrive/helper/SpotlightUtils.kt
+++ appyx-interactions/android/src/androidTest/kotlin/com/bumble/appyx/interactions/sample/testdrive/helper/SpotlightUtils.kt
@@ -8,7 +8,7 @@ import com.bumble.appyx.interactions.sample.NavTarget
 import com.bumble.appyx.interactions.setupSpotlight
 import com.bumble.appyx.transitionmodel.spotlight.Spotlight
 import com.bumble.appyx.transitionmodel.spotlight.SpotlightModel
-import com.bumble.appyx.transitionmodel.spotlight.interpolator.SpotlightSlider
+import com.bumble.appyx.transitionmodel.spotlight.ui.slider.SpotlightSlider
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 
