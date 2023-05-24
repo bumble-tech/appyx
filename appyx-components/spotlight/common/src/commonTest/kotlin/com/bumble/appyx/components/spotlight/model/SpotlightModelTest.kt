051446105aab976b8f465d82d13e060e0d1c0573
andreykovalev <andrey.kovalev@team.bumble.com>
2023-05-24 10:24:38 +0100

    Fix bug when updating elements with different size compared to the current set of elements

--- appyx-components/spotlight/common/src/commonTest/kotlin/com/bumble/appyx/components/spotlight/model/SpotlightModelTest.kt
+++ appyx-components/spotlight/common/src/commonTest/kotlin/com/bumble/appyx/components/spotlight/model/SpotlightModelTest.kt
@@ -1,14 +1,15 @@
-package com.bumble.appyx.components.spotlight.restore
+package com.bumble.appyx.components.spotlight.model
 
 import com.bumble.appyx.components.spotlight.InteractionTarget.Child1
 import com.bumble.appyx.components.spotlight.InteractionTarget.Child2
 import com.bumble.appyx.components.spotlight.SpotlightModel
 import com.bumble.appyx.components.spotlight.operation.Next
+import com.bumble.appyx.components.spotlight.operation.UpdateElements
 import com.bumble.appyx.interactions.core.state.MutableSavedStateMapImpl
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-class SpotlightRestoreTest {
+class SpotlightModelTest {
 
     @Test
     fun GIVEN_spotlight_with_2_elements_WHEN_state_restored_THEN_all_elements_are_retained() {
@@ -31,6 +32,34 @@ class SpotlightRestoreTest {
         assertEquals(
             spotlight.output.value.currentTargetState,
             newSpotlight.output.value.currentTargetState
+        )
+    }
+
+    @Test
+    fun GIVEN_empty_spotlight_WHEN_updated_with_element_THEN_state_contains_element_and_sets_index() {
+        val spotlight = SpotlightModel(
+            items = listOf(),
+            savedStateMap = null
+        )
+
+        val newElements = listOf(Child1, Child2)
+        val newActiveIndex = 1f
+
+        spotlight.operation(
+            UpdateElements(
+                items = newElements,
+                initialActiveIndex = newActiveIndex
+            )
+        )
+
+        assertEquals(
+            expected = newElements,
+            actual = spotlight.elements.value.map { it.interactionTarget }.toList(),
+        )
+
+        assertEquals(
+            expected = newActiveIndex,
+            actual = spotlight.output.value.currentTargetState.activeIndex,
         )
     }
 }
