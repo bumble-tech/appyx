0fe06cbb2fea076869807525fed36dbbed965f19
Lachlan McKee <lachlan.mckee@team.bumble.com>
2023-07-29 11:00:54 +0100

    Ensure detekt can be run separately for plugin jobs

--- plugins/build.gradle.kts
+++ plugins/build.gradle.kts
@@ -1,5 +1,7 @@
 val buildTask = tasks.register("buildPlugins")
+val detektTask = tasks.register("detekt")
 
 subprojects {
     buildTask.configure { dependsOn(tasks.named("build")) }
+    detektTask.configure { dependsOn(tasks.named("detekt")) }
 }
