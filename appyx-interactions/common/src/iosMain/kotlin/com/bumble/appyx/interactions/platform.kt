e3d0780542f2a21ebe880b0a03e8be065b2c9c09
Aashay Gaikwad <aashay.gaikwad@team.bumble.com>
2023-08-16 13:24:00 +0100

    Added iOS implementations for expect functions

--- appyx-interactions/common/src/iosMain/kotlin/com/bumble/appyx/interactions/platform.kt
+++ appyx-interactions/common/src/iosMain/kotlin/com/bumble/appyx/interactions/platform.kt
@@ -3,10 +3,3 @@ package com.bumble.appyx.interactions
 actual fun getPlatformName(): String {
     return "iOS"
 }
-
-actual annotation class Parcelize
-
-actual interface Parcelable
-
-@Target(AnnotationTarget.TYPE)
-actual annotation class RawValue
