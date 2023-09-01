9322a3b5d6323a76302febeaebe6e6124ca9f206
andreykovalev <andrey.kovalev@team.bumble.com>
2023-09-01 14:32:18 +0100

    Move androidLifecycle to NodeExt.kt

--- appyx-navigation/common/src/androidMain/kotlin/com/bumble/appyx/navigation/node/NodeExt.kt
+++ appyx-navigation/common/src/androidMain/kotlin/com/bumble/appyx/navigation/node/NodeExt.kt
@@ -1,6 +1,6 @@
-package com.bumble.appyx.navigation.platform
+package com.bumble.appyx.navigation.node
 
-import com.bumble.appyx.navigation.node.Node
+import com.bumble.appyx.navigation.platform.PlatformLifecycleRegistry
 
 val Node.androidLifecycle: androidx.lifecycle.Lifecycle
     get() = (lifecycle as PlatformLifecycleRegistry).androidLifecycleRegistry
