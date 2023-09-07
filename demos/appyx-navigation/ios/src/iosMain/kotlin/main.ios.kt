fa56c4ae4de7a74043fa24b24a6ec638b4900c1b
Aashay Gaikwad <aashay.gaikwad@team.bumble.com>
2023-09-07 11:52:00 +0100

    Added back icon

--- demos/appyx-navigation/ios/src/iosMain/kotlin/main.ios.kt
+++ demos/appyx-navigation/ios/src/iosMain/kotlin/main.ios.kt
@@ -1,28 +1,63 @@
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.window.ComposeUIViewController
+import androidx.compose.ui.zIndex
 import com.bumble.appyx.components.backstack.BackStack
 import com.bumble.appyx.components.backstack.BackStackModel
-import com.bumble.appyx.components.backstack.ui.parallax.BackStackParallax
+import com.bumble.appyx.components.backstack.operation.pop
+import com.bumble.appyx.components.backstack.ui.slider.BackStackSlider
 import com.bumble.appyx.navigation.navigation.IOSNodeHost
 import com.bumble.appyx.navigation.node.container.ContainerNode
 import com.bumble.appyx.navigation.ui.AppyxSampleAppTheme
 
 fun MainViewController() = ComposeUIViewController {
    AppyxSampleAppTheme {
-       IOSNodeHost(
-           modifier = Modifier
-       ) { buildContext ->
-           ContainerNode(
-               buildContext = buildContext,
-               backStack = BackStack(
-                   model = BackStackModel(
-                       initialTargets = listOf(ContainerNode.InteractionTarget.Selector),
-                       savedStateMap = buildContext.savedStateMap,
-                   ),
-                   motionController = { BackStackParallax(it) },
-                   gestureFactory = { BackStackParallax.Gestures(it) }
-               )
+
+       val backStackModel = remember {
+           BackStackModel<ContainerNode.InteractionTarget>(
+               initialTargets = listOf(ContainerNode.InteractionTarget.Selector),
+               savedStateMap = null,
            )
        }
+       val backStack: BackStack<ContainerNode.InteractionTarget> = BackStack(
+           model = backStackModel,
+           motionController = { BackStackSlider(it) },
+       )
+       val backAction = { backStack.pop() }
+       Box(modifier = Modifier.fillMaxSize()) {
+
+           BackButton(action = backAction)
+
+           IOSNodeHost(
+               modifier = Modifier
+           ) { buildContext ->
+               ContainerNode(
+                   buildContext = buildContext,
+                   backStack = backStack,
+               )
+           }
+       }
    }
+}
+
+@Composable
+private fun BackButton(action: () -> Unit) {
+    IconButton(
+        onClick =  { action.invoke() },
+        modifier = Modifier.zIndex(5f)
+    ) {
+        Icon(
+            imageVector = Icons.Default.ArrowBack,
+            tint = Color.White,
+            contentDescription = ""
+        )
+    }
 }
