9393e85da3f8bbbcbfbdd9535ede3908dc0930ab
Aashay Gaikwad <gaikwadaashayp@gmail.com>
2023-09-18 19:52:07 +0100

    Black background for insets

--- demos/appyx-navigation/ios/src/iosMain/kotlin/main.ios.kt
+++ demos/appyx-navigation/ios/src/iosMain/kotlin/main.ios.kt
@@ -1,19 +1,19 @@
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ComposeUIViewController
 import androidx.compose.ui.zIndex
-import com.bumble.appyx.components.backstack.BackStack
-import com.bumble.appyx.components.backstack.BackStackModel
-import com.bumble.appyx.components.backstack.ui.slider.BackStackSlider
 import com.bumble.appyx.navigation.integration.IOSNodeHost
 import com.bumble.appyx.navigation.node.container.ContainerNode
 import com.bumble.appyx.navigation.ui.AppyxSampleAppTheme
@@ -21,25 +21,37 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
+import platform.UIKit.UIWindow
+import platform.UIKit.safeAreaInsets
 
 val backEvents: Channel<Unit> = Channel()
 
 fun MainViewController() = ComposeUIViewController {
     AppyxSampleAppTheme {
-        Box(modifier = Modifier.fillMaxSize()) {
-            BackButton(rememberCoroutineScope())
+        val coroutineScope = rememberCoroutineScope()
+        Scaffold(
+            modifier = Modifier
+                .background(Color.Black)
+                .padding(top = UIWindow().safeAreaInsets.size.dp)
+        ) {
+            Box {
+                BackButton(coroutineScope)
 
-            IOSNodeHost(
-                modifier = Modifier,
-                onBackPressedEvents = backEvents.receiveAsFlow()
-            ) { buildContext ->
-                ContainerNode(
-                    buildContext = buildContext,
-                )
+                Box(modifier = Modifier.fillMaxSize()) {
+                    IOSNodeHost(
+                        modifier = Modifier,
+                        onBackPressedEvents = backEvents.receiveAsFlow()
+                    ) { buildContext ->
+                        ContainerNode(
+                            buildContext = buildContext,
+                        )
+                    }
+                }
             }
         }
     }
 }
+
 
 @Composable
 private fun BackButton(coroutineScope: CoroutineScope) {
@@ -49,7 +61,7 @@ private fun BackButton(coroutineScope: CoroutineScope) {
                 backEvents.send(Unit)
             }
         },
-        modifier = Modifier.zIndex(5f)
+        modifier = Modifier.zIndex(99f)
     ) {
         Icon(
             imageVector = Icons.Default.ArrowBack,
