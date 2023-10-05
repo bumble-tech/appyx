f1b39f631f2001f852cf0076449e60f54be7d5ea
Zsolt Kocsi <zsolt.kocsi@team.bumble.com>
2023-10-05 22:04:53 +0100

    Imlement nav config for :demos:appyx-navigation

---
+++ demos/appyx-navigation/common/src/commonMain/kotlin/com/bumble/appyx/navigation/node/container/MainNavNode.kt
@@ -0,0 +1,15 @@
+package com.bumble.appyx.navigation.node.container
+
+import com.bumble.appyx.navigation.MainNavItem
+import com.bumble.appyx.navigation.modality.BuildContext
+import com.bumble.appyx.utils.material.AppyxMaterial3NavNode
+
+
+class MainNavNode(
+    buildContext: BuildContext,
+) : AppyxMaterial3NavNode<MainNavItem>(
+    buildContext = buildContext,
+    navTargets = MainNavItem.values().toList(),
+    navTargetResolver = MainNavItem.resolver
+) {
+}
