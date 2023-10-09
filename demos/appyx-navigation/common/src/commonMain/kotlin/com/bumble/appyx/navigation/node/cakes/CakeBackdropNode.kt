0194e4be921c614c0a07c58bbe32400f0f72790b
Zsolt Kocsi <zsolt.kocsi@team.bumble.com>
2023-10-09 22:33:14 +0100

    Implement split carousel with cakes

--- demos/appyx-navigation/common/src/commonMain/kotlin/com/bumble/appyx/navigation/node/cakes/CakeBackdropNode.kt
+++ demos/appyx-navigation/common/src/commonMain/kotlin/com/bumble/appyx/navigation/node/cakes/CakeBackdropNode.kt
@@ -7,20 +7,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.bumble.appyx.interactions.core.ui.property.motionPropertyRenderValue
-import com.bumble.appyx.navigation.colors
 import com.bumble.appyx.navigation.modality.BuildContext
 import com.bumble.appyx.navigation.node.Node
 import com.bumble.appyx.navigation.node.cakes.component.spotlighthero.visualisation.property.HeroProgress
 import com.bumble.appyx.navigation.node.cakes.model.Cake
 
-class CakeDetailsNode(
+class CakeBackdropNode(
     buildContext: BuildContext,
     private val cake: Cake,
     private val onClick: () -> Unit,
@@ -30,14 +28,12 @@ class CakeDetailsNode(
 
     @Composable
     override fun View(modifier: Modifier) {
-        val backgroundColorIdx = rememberSaveable { colors.shuffled().indices.random() }
-        val backgroundColor = colors[backgroundColorIdx]
         val heroProgress = motionPropertyRenderValue<Float, HeroProgress>() ?: 0f
 
         Box(
             modifier = modifier
                 .fillMaxSize()
-                .background(backgroundColor)
+                .background(Color(cake.backgroundColor))
                 .clickable { onClick() }
                 .padding(24.dp)
         ) {
--- demos/appyx-navigation/common/src/commonMain/kotlin/com/bumble/appyx/navigation/node/cakes/CakeListNode.kt
+++ demos/appyx-navigation/common/src/commonMain/kotlin/com/bumble/appyx/navigation/node/cakes/CakeListNode.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import com.bumble.appyx.interactions.core.model.plus
 import com.bumble.appyx.navigation.composable.AppyxComponent
 import com.bumble.appyx.navigation.modality.BuildContext
 import com.bumble.appyx.navigation.node.Node
@@ -26,6 +27,7 @@ import com.bumble.appyx.navigation.node.cakes.component.spotlighthero.SpotlightH
 import com.bumble.appyx.navigation.node.cakes.component.spotlighthero.operation.toggleHeroMode
 import com.bumble.appyx.navigation.node.cakes.component.spotlighthero.visualisation.SpotlightHeroGestures
 import com.bumble.appyx.navigation.node.cakes.component.spotlighthero.visualisation.backdrop.SpotlightHeroBackdropVisualisation
+import com.bumble.appyx.navigation.node.cakes.component.spotlighthero.visualisation.main.SpotlightHeroMainVisualisation
 import com.bumble.appyx.navigation.node.cakes.model.Cake
 import com.bumble.appyx.navigation.node.cakes.model.cakes
 import com.bumble.appyx.utils.multiplatform.Parcelable
@@ -35,39 +37,63 @@ private val animationSpec = spring<Float>(stiffness = Spring.StiffnessLow)
 
 class CakeListNode(
     buildContext: BuildContext,
-    private val spotlight: SpotlightHero<NavTarget> =
+    private val spotlightBackDrop: SpotlightHero<NavTarget> =
         SpotlightHero(
             model = SpotlightHeroModel(
-                items = cakes.map { NavTarget.CakeDetails(it) },
+                items = cakes.map { NavTarget.Backdrop(it) },
                 initialActiveIndex = 0f,
                 savedStateMap = buildContext.savedStateMap
             ),
-            animationSpec = spring(stiffness = Spring.StiffnessLow),
+            animationSpec = animationSpec,
             visualisation = { SpotlightHeroBackdropVisualisation(it) },
+            gestureFactory = { SpotlightHeroGestures(it) }
+        ),
+    private val spotlightMain: SpotlightHero<NavTarget> =
+        SpotlightHero(
+            model = SpotlightHeroModel(
+                items = cakes.map { NavTarget.CakeImage(it) },
+                initialActiveIndex = 0f,
+                savedStateMap = buildContext.savedStateMap
+            ),
+            animationSpec = animationSpec,
+            visualisation = { SpotlightHeroMainVisualisation(it) },
             gestureFactory = { SpotlightHeroGestures(it) }
         )
 ) : ParentNode<NavTarget>(
     buildContext = buildContext,
-    appyxComponent = spotlight
+    appyxComponent = spotlightBackDrop + spotlightMain
 ) {
 
     sealed class NavTarget : Parcelable {
         @Parcelize
-        data class CakeDetails(
+        data class Backdrop(
+            val cake: Cake
+        ) : NavTarget()
+
+        @Parcelize
+        data class CakeImage(
             val cake: Cake
         ) : NavTarget()
     }
 
     override fun resolve(navTarget: NavTarget, buildContext: BuildContext): Node =
         when (navTarget) {
-            is NavTarget.CakeDetails -> CakeDetailsNode(buildContext, navTarget.cake) {
-                spotlight.toggleHeroMode()
+            is NavTarget.Backdrop -> CakeBackdropNode(buildContext, navTarget.cake) {
+                toggleHeroMode()
+            }
+            is NavTarget.CakeImage -> CakeImageNode(buildContext, navTarget.cake) {
+                toggleHeroMode()
             }
         }
 
+    private fun toggleHeroMode() {
+        spotlightBackDrop.toggleHeroMode()
+        spotlightMain.toggleHeroMode()
+    }
+
     @Composable
     override fun View(modifier: Modifier) {
-        val mode = spotlight.mode.collectAsState(LIST)
+        val mode = spotlightBackDrop.mode.collectAsState(LIST)
         val width by animateFloatAsState(
             targetValue = when (mode.value) {
                 LIST -> 0.6f
@@ -81,11 +107,19 @@ class CakeListNode(
             contentAlignment = Alignment.Center
         ) {
             AppyxComponent(
-                appyxComponent = spotlight,
+                appyxComponent = spotlightBackDrop,
+                draggables = listOf(spotlightBackDrop, spotlightMain),
                 modifier = Modifier
                     .fillMaxWidth(width)
                     .fillMaxHeight(1f)
                     .background(Color.DarkGray)
+            )
+            AppyxComponent(
+                appyxComponent = spotlightMain,
+                draggables = listOf(spotlightBackDrop, spotlightMain),
+                modifier = Modifier
+                    .fillMaxWidth(width)
+                    .fillMaxHeight(1f)
             )
         }
     }
