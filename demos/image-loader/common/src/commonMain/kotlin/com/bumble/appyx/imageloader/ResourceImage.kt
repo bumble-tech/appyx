b36ad24dbffc6700397f2a074373c9f5d0bdf80e
Zsolt Kocsi <zsolt.kocsi@team.bumble.com>
2023-10-10 10:47:56 +0100

    Use Dispatchers.Default

--- demos/image-loader/common/src/commonMain/kotlin/com/bumble/appyx/imageloader/ResourceImage.kt
+++ demos/image-loader/common/src/commonMain/kotlin/com/bumble/appyx/imageloader/ResourceImage.kt
@@ -10,6 +10,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.resource
 
@@ -23,10 +25,11 @@ fun ResourceImage(
 ) {
     var image: ImageBitmap? by remember { mutableStateOf(null) }
     LaunchedEffect(Unit) {
-        image =
+        image = withContext(Dispatchers.Default) {
             resource(path)
                 .readBytes()
                 .toImageBitmap()
+        }
     }
     image?.let {
         Image(
