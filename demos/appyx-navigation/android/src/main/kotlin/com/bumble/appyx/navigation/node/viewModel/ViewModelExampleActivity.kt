1573c1a1623a12914aa1329fa6c875825bcfd8c1
alenafedyakshina <alena.fedyakshina@team.bumble.com>
2023-10-13 17:01:25 +0100

    Extract CounterScreen

--- demos/appyx-navigation/android/src/main/kotlin/com/bumble/appyx/navigation/node/viewModel/ViewModelExampleActivity.kt
+++ demos/appyx-navigation/android/src/main/kotlin/com/bumble/appyx/navigation/node/viewModel/ViewModelExampleActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.viewModels
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -13,7 +14,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
@@ -24,7 +24,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.bumble.appyx.navigation.integration.NodeHost
-import com.bumble.appyx.navigation.modality.BuildContext
 import com.bumble.appyx.navigation.node.node
 import com.bumble.appyx.navigation.platform.AndroidLifecycle
 import com.bumble.appyx.navigation.ui.AppyxSampleAppTheme
@@ -50,51 +49,48 @@ class ViewModelExampleActivity : ViewModelNodeActivity() {
                         integrationPoint = appyxV2IntegrationPoint,
                     ) {
                         node(buildContext = it) { modifier ->
-                            CounterScreen(viewModel)
+                            CounterScreen(
+                                viewModel,
+                                modifier
+                            )
                         }
                     }
                 }
             }
         }
     }
+}
 
-    @Composable
-    private fun CounterScreen(viewModel: ViewModelExample) {
-        val uiState by viewModel.uiState.collectAsState(initial = UiState(0))
+@Composable
+fun CounterScreen(
+    viewModel: ViewModelExample,
+    modifier: Modifier = Modifier
+) {
+    val uiState by viewModel.uiState.collectAsState(initial = UiState(0))
 
-        Column(
-            verticalArrangement = Arrangement.spacedBy(24.dp)
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(24.dp)
+    ) {
+        Text(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            text = "Counter: ${uiState.counter}",
+            fontSize = 45.sp
+        )
+
+        Button(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            onClick = { viewModel.incrementCounter() }
         ) {
-            Text(
-                modifier = Modifier.align(Alignment.CenterHorizontally),
-                text = "Counter: ${uiState.counter}",
-                fontSize = 45.sp
-            )
-
-            Button(
-                modifier = Modifier.align(Alignment.CenterHorizontally),
-                onClick = { viewModel.incrementCounter() }
-            ) {
-                Text("Increment")
-            }
+            Text("Increment")
         }
     }
 }
-
 
 @Preview
 @Composable
 fun DefaultPreview() {
     AppyxSampleAppTheme {
-        var counter = remember { 0 }
-
-        node(buildContext = BuildContext.root(null)) { modifier ->
-            Column {
-                Text(text = "Counter: 0")
-                Button(onClick = { counter++ }) {
-                    Text("Increment")
-                }
-            }
-        }
+        CounterScreen(ViewModelExample())
     }
 }
