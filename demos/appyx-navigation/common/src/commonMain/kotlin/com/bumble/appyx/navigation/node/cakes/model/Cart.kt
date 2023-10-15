e722ed93d391e33893d00095cc3a4c86c34c5b73
Zsolt Kocsi <zsolt.kocsi@team.bumble.com>
2023-10-15 22:53:00 +0100

    Use update

--- demos/appyx-navigation/common/src/commonMain/kotlin/com/bumble/appyx/navigation/node/cakes/model/Cart.kt
+++ demos/appyx-navigation/common/src/commonMain/kotlin/com/bumble/appyx/navigation/node/cakes/model/Cart.kt
@@ -2,6 +2,7 @@ package com.bumble.appyx.navigation.node.cakes.model
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 
 class Cart {
 
@@ -9,8 +10,12 @@ class Cart {
     val items: Flow<Map<Cake, Int>> = mutableItems
 
     fun add(cake: Cake) {
-        mutableItems.value = mutableItems.value.toMutableMap().apply {
-            this[cake] = (this[cake] ?: 0) + 1
+        mutableItems.update {
+            it.toMutableMap().apply {
+                this[cake] = (this[cake] ?: 0) + 1
+            }
+
+            it
         }
     }
 
