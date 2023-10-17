593ac057edf110f4b9c22e9d04db7131d306e18e
alenafedyakshina <alena.fedyakshina@team.bumble.com>
2023-10-17 17:21:12 +0100

    Use ViewModelNode in example

--- demos/appyx-navigation/android/src/main/kotlin/com/bumble/appyx/navigation/node/viewModel/ViewModelNodeExample.kt
+++ demos/appyx-navigation/android/src/main/kotlin/com/bumble/appyx/navigation/node/viewModel/ViewModelNodeExample.kt
@@ -3,22 +3,42 @@ package com.bumble.appyx.navigation.node.viewModel
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.bumble.appyx.navigation.modality.BuildContext
 import com.bumble.appyx.utils.viewmodel.node.ViewModelNode
 
 class ViewModelNodeExample(buildContext: BuildContext) : ViewModelNode(buildContext) {
 
-    private val viewModels = viewModelStore
-
     @Composable
     @Override
     override fun View(modifier: Modifier) {
+        val uiState by viewModel.uiState.collectAsState(initial = UiState(0))
+
         Column(
             modifier = modifier.fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(24.dp)
-        ) {}
+        ) {
+            Text(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                text = "Counter: ${uiState.counter}",
+                fontSize = 45.sp
+            )
+
+            Button(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                onClick = { viewModel.incrementCounter() }
+            ) {
+                Text("Increment")
+            }
+
+        }
     }
 }
