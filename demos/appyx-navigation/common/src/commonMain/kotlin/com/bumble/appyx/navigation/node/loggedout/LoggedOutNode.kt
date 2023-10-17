844208c23aa8bee027df959cefa1f786f4650e04
Zsolt Kocsi <zsolt.kocsi@team.bumble.com>
2023-10-17 18:10:51 +0100

    Tweak layout

--- demos/appyx-navigation/common/src/commonMain/kotlin/com/bumble/appyx/navigation/node/loggedout/LoggedOutNode.kt
+++ demos/appyx-navigation/common/src/commonMain/kotlin/com/bumble/appyx/navigation/node/loggedout/LoggedOutNode.kt
@@ -1,11 +1,13 @@
 package com.bumble.appyx.navigation.node.loggedout
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -15,9 +17,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.bumble.appyx.components.backstack.BackStack
 import com.bumble.appyx.components.backstack.BackStackModel
-import com.bumble.appyx.components.backstack.operation.replace
+import com.bumble.appyx.components.backstack.operation.push
 import com.bumble.appyx.components.backstack.ui.slider.BackStackSlider
 import com.bumble.appyx.navigation.composable.AppyxComponent
 import com.bumble.appyx.navigation.modality.BuildContext
@@ -71,18 +74,20 @@ class LoggedOutNode(
     }
 
     @Composable
-    private fun SplashScreen(modifier: Modifier) {
+    private fun SplashScreen(modifier: Modifier = Modifier) {
         Column(
             modifier = modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = "Splash",
+                text = "Welcome!",
+                style = MaterialTheme.typography.headlineLarge
             )
+            Spacer(modifier = Modifier.size(24.dp))
             Button(
                 onClick = {
-                    backStack.replace(NavTarget.Login)
+                    backStack.push(NavTarget.Login)
                 }
             ) {
                 Text(
@@ -94,7 +99,7 @@ class LoggedOutNode(
 
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
-    private fun LoginScreen(modifier: Modifier) {
+    private fun LoginScreen(modifier: Modifier = Modifier) {
         Column(
             modifier = modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
@@ -104,12 +109,15 @@ class LoggedOutNode(
 
             Text(
                 text = "Login",
+                style = MaterialTheme.typography.headlineSmall
             )
+            Spacer(modifier = Modifier.size(24.dp))
             TextField(
                 value = name,
                 onValueChange = { name = it },
                 label = { Text("Name") }
             )
+            Spacer(modifier = Modifier.size(24.dp))
             Button(
                 onClick = {
                     onLogin(
--- demos/appyx-navigation/common/src/commonMain/kotlin/com/bumble/appyx/navigation/node/profile/ProfileNode.kt
+++ demos/appyx-navigation/common/src/commonMain/kotlin/com/bumble/appyx/navigation/node/profile/ProfileNode.kt
@@ -1,14 +1,17 @@
 package com.bumble.appyx.navigation.node.profile
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.bumble.appyx.components.backstack.BackStack
 import com.bumble.appyx.components.backstack.BackStackModel
 import com.bumble.appyx.components.backstack.ui.slider.BackStackSlider
@@ -51,7 +54,9 @@ class ProfileNode(
                 ) {
                     Text(
                         text = "Hello ${user.name}!",
+                        style = MaterialTheme.typography.headlineSmall
                     )
+                    Spacer(modifier = Modifier.size(24.dp))
                     Button(
                         onClick = onLogout
                     ) {
