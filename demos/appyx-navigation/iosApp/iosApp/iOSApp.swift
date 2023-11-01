2aa51d1b7476a5e9d518f12e96279c421ba53176
Mike Pesate <5579996+MikePT28@users.noreply.github.com>
2023-11-01 15:51:09 +0100

    Fix indentation

--- demos/appyx-navigation/iosApp/iosApp/iOSApp.swift
+++ demos/appyx-navigation/iosApp/iosApp/iOSApp.swift
@@ -3,15 +3,15 @@ import ios
 
 @main
 struct iOSApp: App {
-	var body: some Scene {
-		WindowGroup {
-		    ZStack {
-		        Color.white.ignoresSafeArea(.all) // status bar color
-			    ContentView()
+    var body: some Scene {
+        WindowGroup {
+            ZStack {
+                Color.white.ignoresSafeArea(.all) // status bar color
+                ContentView()
             }
             .onOpenURL { incomingURL in
                 Main_iosKt.handleDeepLinks(url: incomingURL)
             }
-		}
-	}
+        }
+    }
 }
