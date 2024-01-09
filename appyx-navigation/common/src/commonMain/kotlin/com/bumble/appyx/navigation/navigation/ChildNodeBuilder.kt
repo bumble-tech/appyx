c287d35674b5ad782791035b45dfc3035186a8d1
Zsolt Kocsi <zsolt.kocsi@team.bumble.com>
2024-01-09 14:02:51 +0000

    Make ChildNodeBuilder a fun interface

--- appyx-navigation/common/src/commonMain/kotlin/com/bumble/appyx/navigation/navigation/ChildNodeBuilder.kt
+++ appyx-navigation/common/src/commonMain/kotlin/com/bumble/appyx/navigation/navigation/ChildNodeBuilder.kt
@@ -3,7 +3,7 @@ package com.bumble.appyx.navigation.navigation
 import com.bumble.appyx.navigation.modality.BuildContext
 import com.bumble.appyx.navigation.node.Node
 
-interface ChildNodeBuilder<T> {
+fun interface ChildNodeBuilder<T> {
 
     fun buildChildNode(reference: T, buildContext: BuildContext): Node
 }
