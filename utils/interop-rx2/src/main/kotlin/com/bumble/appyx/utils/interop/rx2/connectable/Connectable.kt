c85a3c58ece3734274f25a798cf20f16e7e240d5
mapm14 <mapm14@gmail.com>
2024-02-05 15:02:44 +0100

    Add Input caching to Rx implementations and remove NodeLifecycleAware from Connectables

--- utils/interop-rx2/src/main/kotlin/com/bumble/appyx/utils/interop/rx2/connectable/Connectable.kt
+++ utils/interop-rx2/src/main/kotlin/com/bumble/appyx/utils/interop/rx2/connectable/Connectable.kt
@@ -1,9 +1,8 @@
 package com.bumble.appyx.utils.interop.rx2.connectable
 
-import com.bumble.appyx.navigation.plugin.NodeLifecycleAware
 import com.jakewharton.rxrelay2.Relay
 
-interface Connectable<Input, Output> : NodeLifecycleAware {
+interface Connectable<Input, Output> {
     val input: Relay<Input>
     val output: Relay<Output>
 }
--- utils/interop-rx2/src/main/kotlin/com/bumble/appyx/utils/interop/rx2/connectable/NodeConnector.kt
+++ utils/interop-rx2/src/main/kotlin/com/bumble/appyx/utils/interop/rx2/connectable/NodeConnector.kt
@@ -2,61 +2,106 @@ package com.bumble.appyx.utils.interop.rx2.connectable
 
 import android.annotation.SuppressLint
 import com.bumble.appyx.navigation.lifecycle.Lifecycle
+import com.bumble.appyx.navigation.plugin.NodeLifecycleAware
 import com.jakewharton.rxrelay2.PublishRelay
 import com.jakewharton.rxrelay2.Relay
 import io.reactivex.Observer
 
-class NodeConnector<Input: Any, Output: Any>(
-    override val input: Relay<Input> = PublishRelay.create(),
-) : Connectable<Input, Output> {
+class NodeConnector<Input : Any, Output : Any> : Connectable<Input, Output>, NodeLifecycleAware {
+
+    override fun onCreate(lifecycle: Lifecycle) {
+        flushInputCache()
+        flushOutputCache()
+    }
+
+    private val intakeInput: Relay<Input> = PublishRelay.create()
+    private val exhaustInput: Relay<Input> = PublishRelay.create()
+    private var isInputFlushed = false
+    private val inputCache = mutableListOf<Input>()
+
+    override val input: Relay<Input> = object : Relay<Input>() {
+
+        override fun subscribeActual(observer: Observer<in Input>) {
+            exhaustInput.subscribe(observer)
+        }
+
+        override fun accept(value: Input) {
+            intakeInput.accept(value)
+        }
+
+        override fun hasObservers() = exhaustInput.hasObservers()
+
+    }
+
+    private val inputCacheSubscription = intakeInput.subscribe {
+        synchronized(this) {
+            if (!isInputFlushed) {
+                inputCache.add(it)
+            } else {
+                exhaustInput.accept(it)
+                switchToInputExhaust()
+            }
+        }
+    }
+
+    private fun flushInputCache() {
+        synchronized(this) {
+            if (isInputFlushed) error("Input already flushed")
+            isInputFlushed = true
+            inputCache.forEach { exhaustInput.accept(it) }
+            inputCache.clear()
+        }
+    }
+
+    @SuppressLint("CheckResult")
+    private fun switchToInputExhaust() {
+        intakeInput.subscribe { exhaustInput.accept(it) }
+        inputCacheSubscription.dispose()
+    }
 
 
-    private val intake: Relay<Output> = PublishRelay.create()
-    private val exhaust: Relay<Output> = PublishRelay.create()
-    private var isFlushed = false
+    private val intakeOutput: Relay<Output> = PublishRelay.create()
+    private val exhaustOutput: Relay<Output> = PublishRelay.create()
+    private var isOutputFlushed = false
     private val outputCache = mutableListOf<Output>()
 
     override val output: Relay<Output> = object : Relay<Output>() {
 
-        override fun subscribeActual(observer: Observer<in Output>?) {
-            exhaust.subscribe(observer as Observer<Output>)
+        override fun subscribeActual(observer: Observer<in Output>) {
+            exhaustOutput.subscribe(observer)
         }
 
         override fun accept(value: Output) {
-            intake.accept(value)
+            intakeOutput.accept(value)
         }
 
-        override fun hasObservers() = exhaust.hasObservers()
+        override fun hasObservers() = exhaustOutput.hasObservers()
 
     }
 
-    override fun onCreate(lifecycle: Lifecycle) {
-        flushOutputCache()
-    }
-
-    private val cacheSubscription = intake.subscribe {
+    private val outputCacheSubscription = intakeOutput.subscribe {
         synchronized(this) {
-            if (!isFlushed) {
+            if (!isOutputFlushed) {
                 outputCache.add(it)
             } else {
-                exhaust.accept(it)
-                switchToExhaust()
+                exhaustOutput.accept(it)
+                switchToOutputExhaust()
             }
         }
     }
 
     private fun flushOutputCache() {
         synchronized(this) {
-            if (isFlushed) error("Already flushed")
-            isFlushed = true
-            outputCache.forEach { exhaust.accept(it) }
+            if (isOutputFlushed) error("Output already flushed")
+            isOutputFlushed = true
+            outputCache.forEach { exhaustOutput.accept(it) }
             outputCache.clear()
         }
     }
 
     @SuppressLint("CheckResult")
-    private fun switchToExhaust() {
-        intake.subscribe { exhaust.accept(it) }
-        cacheSubscription.dispose()
+    private fun switchToOutputExhaust() {
+        intakeOutput.subscribe { exhaustOutput.accept(it) }
+        outputCacheSubscription.dispose()
     }
 }
